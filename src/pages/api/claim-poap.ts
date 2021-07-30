e412936050589f7c520fafee79d115c06c97dc1d
Didier Krux <didier@krux.co>
2021-07-30 13:14:54 +0200

    add comments for claiming POAP

--- src/pages/api/claim-poap.ts
+++ src/pages/api/claim-poap.ts
@@ -12,7 +12,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> {
-  // TODO: first check onchain if the quest has actually been completed
+  // [step 1] TODO: check if the quest action has actually been completed via wallet signature or onchain data (Moralis?)
   const { address, poapEventId } = req.query
   if (
     !address ||
@@ -25,6 +25,7 @@ export default async function handler(
   console.log(address)
   console.log(poapEventId)
 
+  // [step 2] get the list of claiming codes stored in Notion
   await axios
     .get(
       `https://potion-api.vercel.app/table?id=${NOTION_POAP_ID[poapEventId]}`
@@ -32,6 +33,7 @@ export default async function handler(
     .then(async function (response) {
       const poapLinks: { fields: { claim: string; address?: string } }[] =
         response.data
+      // [step 3] check how many POAPs have already been claimed for this quest
       // TODO: replace with tokensQuantityByEventId https://github.com/poap-xyz/poap-webapp/blob/2def482ffec93e6cbc4e3c5e5a18000805cc6c2b/src/api.ts#L1235
       await axios
         .post('https://api.thegraph.com/subgraphs/name/poap-xyz/poap-xdai', {
@@ -43,6 +45,7 @@ export default async function handler(
           const qr_hash =
             poapLinks[response.data.data.event?.tokenCount | 0].fields.claim
           console.log(qr_hash)
+          // [step 4] get the secret code from POAP api
           await axios
             .get(`https://api.poap.xyz/actions/claim-qr?qr_hash=${qr_hash}`)
             .then(async function (response) {
@@ -54,6 +57,7 @@ export default async function handler(
                 secret,
               }
               console.log(data)
+              // [step 5] claim the POAP
               await axios
                 .post('https://api.poap.xyz/actions/claim-qr', data)
                 .then(async function (response) {
