c501bbf63624a543718ee8f2ba0d6ca46556a1c9
Didier Krux <didier@krux.co>
2021-08-06 16:30:34 +0200

    clean code

--- src/pages/feedback.tsx
+++ src/pages/feedback.tsx
@@ -1,18 +1,13 @@
 import React from 'react'
 import { Box, Container } from '@chakra-ui/react'
-import styled from '@emotion/styled'
 import { GetStaticProps } from 'next'
 
 import { PageMetaProps } from 'components/Head'
-
-// TODO LATER: rename back to index.tsx
 
 const pageMeta: PageMetaProps = {
   title: 'Feedback',
   description: '...',
 }
-
-const Iframe = styled.iframe``
 
 export const getStaticProps: GetStaticProps = async () => {
   return {
@@ -30,12 +25,12 @@ const Feedback = (): JSX.Element => {
         padding="4"
         backgroundColor="white"
       >
-        <Iframe
+        <iframe
           src="https://tally.so/embed/wvy7A3?hideTitle=0&alignLeft=1"
           frameBorder="0"
           width="100%"
           height="100%"
-        ></Iframe>
+        ></iframe>
       </Box>
     </Container>
   )
