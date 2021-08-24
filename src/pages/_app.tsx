4c8b3e8adb94e94b78e89b6e4b9fa9a831e6c819
Didier Krux <didier@krux.co>
2021-08-24 02:54:06 +0200

    add umami tracking

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app'
 import { Web3ReactProvider } from '@web3-react/core'
 import { ethers } from 'ethers'
 import 'swiper/swiper-bundle.min.css'
+import Head from 'next/head'
 
 import dynamic from 'next/dynamic'
 import Layout from 'layout'
@@ -19,6 +20,15 @@ const Web3ReactProviderDefault = dynamic(
 const App = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
     <>
+      <Head>
+        {/* TODO: separate tracking for dev and prod + add data-website-id to .env */}
+        <script
+          async
+          defer
+          data-website-id="62d1cf48-425d-4658-9b86-3eea78ac9714"
+          src="https://bankless-umami.vercel.app/umami.js"
+        />
+      </Head>
       <noscript>You need to enable JavaScript to run this app.</noscript>
       <ThemeProvider>
         <Web3ReactProvider getLibrary={getLibrary}>
