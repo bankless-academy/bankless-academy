a7667f879f1b9a2805335c61b1e965accf0eb5b8
Didier Krux <didier@krux.co>
2021-08-27 14:55:34 +0200

    strip parentheses content (slide numbers)

--- import-content.js
+++ import-content.js
@@ -39,7 +39,7 @@ axios
                 [KEY_MATCHING[k]]: Number.isNaN(parseInt(course.fields[k]))
                   ? course.fields[k]
                   : // transform to number if the string contains a number
-                    parseInt(course.fields[k]),
+                  parseInt(course.fields[k]),
               }),
             {}
           )
@@ -51,13 +51,14 @@ axios
             .replace(/-+/g, '-') // collapse dashes
           const content = JSON.parse(
             `[` +
-              response.data
-                .replace(/"/g, "'")
-                .replace(/\s+/g, ' ') // collapse whitespace
-                .replace(/<h1>/g, `"},{"type": "LEARN","title": "`)
-                .replace(/<\/h1>/g, `","content": "`)
-                .substr(3) + // remove extra "}, at the beginning
-              `"}]`
+            response.data
+              .replace(/"/g, "'")
+              .replace(/ *\([^)]*\) */g, "") // strip parentheses content (slide numbers)
+              .replace(/\s+/g, ' ') // collapse whitespace
+              .replace(/<h1>/g, `"},{"type": "LEARN","title": "`)
+              .replace(/<\/h1>/g, `","content": "`)
+              .substr(3) + // remove extra "}, at the beginning
+            `"}]`
           )
           let quizNb = 0
           const slides = content.map((slide) => {
