c3e50f8f98e35217f7d40d7b8f9b366976b49031
Didier Krux <didier@krux.co>
2021-09-01 19:04:12 +0200

    import-poaps script

--- knexfile.js
+++ knexfile.js
@@ -6,6 +6,6 @@ module.exports = {
     host: process.env.DB_HOST | 'localhost',
     user: process.env.DB_USER | 'postgres',
     password: process.env.DB_PASSWORD | '',
-    database: process.env.DB_DATABASE | 'postgres'
-  }
-};
+    database: process.env.DB_DATABASE | 'postgres',
+  },
+}
