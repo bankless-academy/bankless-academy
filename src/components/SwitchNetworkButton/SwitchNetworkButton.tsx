8601d3d058067242cfee7a4b79df8cfabe27d3be
Sweets Sweetman <sweetmantech@gmail.com>
2021-09-01 15:50:04 +0700

    I abstract switchNetwork to it's own microservice function.

--- src/components/SwitchNetworkButton/SwitchNetworkButton.tsx
+++ src/components/SwitchNetworkButton/SwitchNetworkButton.tsx
@@ -2,55 +2,13 @@ import React from 'react'
 import { Select } from '@chakra-ui/react'
 import networks from 'constants/networks'
 import { useState } from 'react'
+import switchNetwork from './switchNetwork'
 
 const SwitchNetworkButton = ({ isMobile }: { isMobile: boolean }): any => {
   const [network, setNetwork] = useState(networks.mainnet)
   const handleChange = async (event: React.ChangeEvent<HTMLSelectElement>) => {
     const networkName = event.target.value
-    await switchNetwork(networkName.toLowerCase())
-  }
-
-  const switchNetwork = async (networkName) => {
-    const { ethereum } = window
-
-    try {
-      await ethereum.request({
-        method: 'wallet_switchEthereumChain',
-        params: [
-          { chainId: `0x${networks[networkName].chainId.toString(16)}` },
-        ],
-      })
-      setNetwork(networks[networkName])
-    } catch (error) {
-      // This error code indicates that the chain has not been added to MetaMask.
-      if (error.code === 4902) {
-        try {
-          const data = [
-            {
-              chainId: `0x${networks[networkName].chainId.toString(16)}`,
-              chainName: networks[networkName].networkName,
-              nativeCurrency: {
-                name: networks[networkName].currencySymbol,
-                symbol: networks[networkName].currencySymbol,
-                decimals: 18,
-              },
-              rpcUrls: [networks[networkName].rpcUrl],
-              blockExplorerUrls: [networks[networkName].blockExplorer],
-            },
-          ]
-          await ethereum.request({
-            method: 'wallet_addEthereumChain',
-            params: data,
-          })
-          setNetwork(networks[networkName])
-        } catch (addError) {
-          // handle "add" error
-          console.error(addError)
-        }
-      }
-      // handle other "switch" errors
-      console.error(error)
-    }
+    await switchNetwork(networkName.toLowerCase(), setNetwork)
   }
 
   return (
---
+++ src/components/SwitchNetworkButton/switchNetwork.ts
@@ -0,0 +1,47 @@
+import networks from 'constants/networks'
+
+const switchNetwork = async (
+  networkName: string,
+  setNetwork: (a: string) => void
+): Promise<any> => {
+  const { ethereum } = window
+
+  try {
+    await ethereum.request({
+      method: 'wallet_switchEthereumChain',
+      params: [{ chainId: `0x${networks[networkName].chainId.toString(16)}` }],
+    })
+    return setNetwork(networks[networkName])
+  } catch (error) {
+    // This error code indicates that the chain has not been added to MetaMask.
+    if (error.code === 4902) {
+      try {
+        const data = [
+          {
+            chainId: `0x${networks[networkName].chainId.toString(16)}`,
+            chainName: networks[networkName].networkName,
+            nativeCurrency: {
+              name: networks[networkName].currencySymbol,
+              symbol: networks[networkName].currencySymbol,
+              decimals: 18,
+            },
+            rpcUrls: [networks[networkName].rpcUrl],
+            blockExplorerUrls: [networks[networkName].blockExplorer],
+          },
+        ]
+        await ethereum.request({
+          method: 'wallet_addEthereumChain',
+          params: data,
+        })
+        return setNetwork(networks[networkName])
+      } catch (addError) {
+        // handle "add" error
+        return console.error(addError)
+      }
+    }
+    // handle other "switch" errors
+    return console.error(error)
+  }
+}
+
+export default switchNetwork
