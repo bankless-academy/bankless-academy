1c12dead6f9dadd6e9038c1df98853cffcf5fbe4
Didier Krux <didier@krux.co>
2021-09-01 19:25:58 +0200

    add Poaps table

--- knexfile.js
+++ knexfile.js
@@ -3,9 +3,9 @@ require('dotenv').config()
 module.exports = {
   client: 'pg',
   connection: {
-    host: process.env.DB_HOST | 'localhost',
-    user: process.env.DB_USER | 'postgres',
-    password: process.env.DB_PASSWORD | '',
-    database: process.env.DB_DATABASE | 'postgres',
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'postgres',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_DATABASE || 'postgres',
   },
 }
