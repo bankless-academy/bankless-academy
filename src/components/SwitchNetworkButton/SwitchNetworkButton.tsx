2021f54ec0a3aca2488c6f242f49b1157971ac1c
Sweets Sweetman <sweetmantech@gmail.com>
2021-09-06 15:55:13 +0700

    I add images to dropdown Network Switcher

--- src/components/SwitchNetworkButton/SwitchNetworkButton.tsx
+++ src/components/SwitchNetworkButton/SwitchNetworkButton.tsx
@@ -1,14 +1,23 @@
 import React, { useEffect, useState } from 'react'
-import { Image, Select } from '@chakra-ui/react'
+import {
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Image,
+  Grid,
+  GridItem,
+} from '@chakra-ui/react'
+import { ChevronDownIcon } from '@chakra-ui/icons'
 import networks from 'constants/networks'
 import switchNetwork from './switchNetwork'
 import handleNetworkChange from './handleNetworkChange'
 
-const SwitchNetworkButton = ({ isMobile }: { isMobile: boolean }): any => {
+const SwitchNetworkButton = (): any => {
   const [network, setNetwork] = useState(networks.mainnet)
 
-  const handleChange = async (event: React.ChangeEvent<HTMLSelectElement>) => {
-    const networkName = event.target.value
+  const handleChange = async (networkName) => {
     await switchNetwork(networkName.toLowerCase(), setNetwork)
   }
 
@@ -21,17 +30,38 @@ const SwitchNetworkButton = ({ isMobile }: { isMobile: boolean }): any => {
 
   return (
     <div>
-      <Select
-        size={isMobile ? 'sm' : 'md'}
-        onChange={handleChange}
-        value={network.name}
-        icon={<Image src={network.image} />}
-      >
-        <option value={networks.mainnet.name}>{networks.mainnet.name}</option>
-        <option value={networks.kovan.name}>{networks.kovan.name}</option>
-        <option value={networks.matic.name}>{networks.matic.name}</option>
-        <option value={networks.mumbai.name}>{networks.mumbai.name}</option>
-      </Select>
+      <Menu>
+        <MenuButton as={Button}>
+          <Grid templateColumns="repeat(4, 1fr)">
+            <GridItem colSpan={1}>
+              <Image src={network.image} height={22} mr="12px" />
+            </GridItem>
+            <GridItem colSpan={2}>{network.name}</GridItem>
+            <GridItem colSpan={1}>
+              <ChevronDownIcon />
+            </GridItem>
+          </Grid>
+        </MenuButton>
+        <MenuList>
+          {Object.keys(networks).map((network, index) => (
+            <MenuItem
+              key={index}
+              minH="40px"
+              onClick={() => handleChange(networks[network].name)}
+            >
+              <Image
+                height={25}
+                width={25}
+                rounded="full"
+                src={networks[network].image}
+                alt={networks[network].name}
+                mr="12px"
+              />
+              <span>{networks[network].name}</span>
+            </MenuItem>
+          ))}
+        </MenuList>
+      </Menu>
     </div>
   )
 }
