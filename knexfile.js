1220621c2f4930f08e03c7900836ff6296e2e628
Didier Krux <didier@krux.co>
2021-09-23 16:27:36 +0300

    add DB_PORT env var

--- knexfile.js
+++ knexfile.js
@@ -4,6 +4,7 @@ module.exports = {
   client: 'pg',
   connection: {
     host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || '5432',
     user: process.env.DB_USER || 'postgres',
     password: process.env.DB_PASSWORD || '',
     database: process.env.DB_DATABASE || 'postgres',
