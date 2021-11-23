09c35080c0074dc3fa14a95ac5c0b8d3529950ce
Didier Krux <didier@krux.co>
2021-11-24 00:35:17 +0100

    clean & update nav

--- src/layout/Nav.tsx
+++ src/layout/Nav.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {
-  // Box,
+  Box,
   Button,
   HStack,
   Spacer,
@@ -9,25 +9,10 @@ import {
 } from '@chakra-ui/react'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import styled from '@emotion/styled'
 
-// import ColorModeSwitcher from 'layout/ColorModeSwitcher'
-// import LanguageModeSwitcher from 'layout/LanguageModeSwitcher'
 import ConnectWalletButton from 'components/ConnectWalletButton'
 import SwitchNetworkButton from 'components/SwitchNetworkButton/'
 import { PROJECT_NAME } from 'constants/index'
-
-const MenuConnect = styled(HStack)<{ isSmallScreen?: boolean }>`
-  ${(props) =>
-    props.isSmallScreen &&
-    `
-    position: fixed;
-    bottom: 0;
-    left: 0;
-    padding: 1rem;
-    width: 100%;
-  `}
-`
 
 const Nav: React.FC = () => {
   const [isSmallScreen] = useMediaQuery('(max-width: 800px)')
@@ -36,34 +21,9 @@ const Nav: React.FC = () => {
 
   return (
     <header>
-      <nav>
+      <Box bgColor="black" borderBottom="1px solid #222222">
         <Flex p={4}>
           <HStack spacing={4}>
-            {/* <Link href="/">
-              <Button
-                isActive={currentLink === '/'}
-                variant="unstyled"
-                size={isSmallScreen ? 'sm' : 'md'}
-              >
-                ⚡️ {PROJECT_NAME}
-              </Button>
-            </Link>
-            <Link href="/quests">
-              <Button
-                isActive={currentLink.includes('/quest')}
-                size={isSmallScreen ? 'sm' : 'md'}
-              >
-                Quests
-              </Button>
-            </Link>
-            <Link href="/about">
-              <Button
-                isActive={currentLink === '/about'}
-                size={isSmallScreen ? 'sm' : 'md'}
-              >
-                About
-              </Button>
-            </Link> */}
             <Link href="/">
               <Button
                 isActive={currentLink === '/'}
@@ -75,28 +35,22 @@ const Nav: React.FC = () => {
                 display="flex"
                 fontSize="xl"
               >
-                {PROJECT_NAME}
+                {!isSmallScreen && PROJECT_NAME}
               </Button>
             </Link>
           </HStack>
           <Spacer />
-          <MenuConnect
+          <HStack
             zIndex="toast"
             spacing={4}
             isSmallScreen={isSmallScreen}
-            // TODO: auto change via theme
-            bgColor={isSmallScreen ? 'gray.200' : 'auto'}
             justifyContent="space-between"
           >
             <SwitchNetworkButton isSmallScreen={isSmallScreen} />
             <ConnectWalletButton isSmallScreen={isSmallScreen} />
-            {/* <Box display="flex">
-              <LanguageModeSwitcher size={isSmallScreen ? 'sm' : 'md'} />
-              <ColorModeSwitcher size={isSmallScreen ? 'sm' : 'md'} />
-            </Box> */}
-          </MenuConnect>
+          </HStack>
         </Flex>
-      </nav>
+      </Box>
     </header>
   )
 }
