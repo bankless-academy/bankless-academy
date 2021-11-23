6df3b635257d83e47d26dadfa33d70b01ae8e9d4
Didier Krux <didier@krux.co>
2021-11-24 00:34:51 +0100

    update bg color

--- src/layout/PageBackground.tsx
+++ src/layout/PageBackground.tsx
@@ -1,17 +1,13 @@
 import * as React from 'react'
-import { useColorMode, Box, BoxProps } from '@chakra-ui/react'
+import { Box, BoxProps } from '@chakra-ui/react'
 
 // TODO: move this to theme if possible
 
 const PageBackground: React.FC<BoxProps> = (props) => {
-  const { colorMode } = useColorMode()
-
   return (
     <Box
       bg={
-        colorMode === 'dark'
-          ? 'radial-gradient(233.98% 233.98% at -6.23% -45.01%, #FA8583 0%, #473E5B 58.32%, #332B43 91.94%)'
-          : 'aliceblue'
+        'radial-gradient(233.98% 233.98% at -6.23% -45.01%, #473E5B 0%, #332B43 91.94%)'
       }
       {...props}
     />
