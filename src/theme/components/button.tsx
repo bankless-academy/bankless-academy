7e7ab5f721c0e1da56ddc1155dff020659242c3c
Didier Krux <didier@krux.co>
2021-11-24 00:34:35 +0100

    update button styles

--- src/theme/components/button.tsx
+++ src/theme/components/button.tsx
@@ -1,61 +1,88 @@
 export default {
   Button: {
-    baseStyle: {
-      'border-radius': '4px',
-    },
+    baseStyle: {},
     variants: {
-      solid: {
+      primary: {
         background:
-          'radial-gradient(156.09% 261.49% at 100% 15.62%, #8F4AE7 0%, #EB77FE 51.56%, #FF87A1 100%)',
-        border: '1px solid transparent',
+          'linear-gradient(135.91deg, #B06FD8 29.97%, #597AEE 99.26%)',
+        backdropFilter: 'blur(50px)',
+        borderRadius: '8px',
         _hover: {
+          paddingLeft: '15px',
+          paddingRight: '15px',
           border: '1px solid #B85FF1',
           'box-sizing': 'border-box',
           'backdrop-filter': 'blur(42px)',
           background:
-            'radial-gradient(156.09% 261.49% at 100% 15.62%, #8F4AE7 0%, #EB77FE 51.56%, #FF87A1 100%)',
+            'linear-gradient(84.62deg, #B06FD8 7.42%, #597AEE 218.41%)',
           bgClip: 'text',
           color: 'transparent',
           span: {
             path: {
-              fill: '#B85FF1',
-            },
-          },
-        },
-        _press: {
-          border: '1px solid #B85FF1',
-          'box-sizing': 'border-box',
-          'backdrop-filter': 'blur(42px)',
-          background:
-            'radial-gradient(156.09% 261.49% at 100% 15.62%, #8F4AE7 0%, #EB77FE 51.56%, #FF87A1 100%)',
-          bgClip: 'text',
-          color: 'transparent',
-          span: {
-            path: {
-              fill: '#B85FF1',
-            },
-          },
-        },
-        _active: {
-          border: '1px solid #B85FF1',
-          'box-sizing': 'border-box',
-          'backdrop-filter': 'blur(42px)',
-          background:
-            'radial-gradient(156.09% 261.49% at 100% 15.62%, #8F4AE7 0%, #EB77FE 51.56%, #FF87A1 100%)',
-          bgClip: 'text',
-          color: 'transparent',
-          span: {
-            path: {
-              fill: '#B85FF1',
+              fill: '#5c79ed',
             },
           },
         },
       },
-      primary: {},
-      outline: {
-        border: '1px solid #B85FF1',
-        'box-sizing': 'border-box',
-        'backdrop-filter': 'blur(42px)',
+      primaryBig: {
+        background:
+          'linear-gradient(135.91deg, #B06FD8 29.97%, #597AEE 99.26%)',
+        backdropFilter: 'blur(50px)',
+        borderRadius: '60px',
+        _hover: {
+          paddingLeft: '23px',
+          paddingRight: '23px',
+          border: '1px solid #B85FF1',
+          'box-sizing': 'border-box',
+          'backdrop-filter': 'blur(42px)',
+          background:
+            'linear-gradient(84.62deg, #B06FD8 7.42%, #597AEE 218.41%)',
+          bgClip: 'text',
+          color: 'transparent',
+          span: {
+            path: {
+              fill: '#5c79ed',
+            },
+          },
+        },
+      },
+      secondary: {
+        background: '#3F3253',
+        backdropFilter: 'blur(50px)',
+        borderRadius: '8px',
+        _hover: {
+          paddingLeft: '15px',
+          paddingRight: '15px',
+          background: 'transparent',
+          border: '1px solid #F2E5FF',
+          'box-sizing': 'border-box',
+          'backdrop-filter': 'blur(42px)',
+          color: '#F2E5FF',
+          span: {
+            path: {
+              fill: '#F2E5FF',
+            },
+          },
+        },
+      },
+      secondaryBig: {
+        background: '#3F3253',
+        backdropFilter: 'blur(50px)',
+        borderRadius: '60px',
+        _hover: {
+          paddingLeft: '23px',
+          paddingRight: '23px',
+          background: 'transparent',
+          border: '1px solid #F2E5FF',
+          'box-sizing': 'border-box',
+          'backdrop-filter': 'blur(42px)',
+          color: '#F2E5FF',
+          span: {
+            path: {
+              fill: '#F2E5FF',
+            },
+          },
+        },
       },
     },
   },
