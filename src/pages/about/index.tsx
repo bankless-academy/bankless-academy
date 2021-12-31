90b9754a00eed6f9f59114c0133f9c1a43b97870
Austin <austin.foss.account@protonmail.com>
2021-12-30 18:11:52 -0700

    Implementing PullReq#54 File change #6: Moved about.tsx into new directory 'about' and renamed file index.tsx to match

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,10 +1,9 @@
 import { GetStaticProps } from 'next'
 
-import { PageMetaProps } from 'components/Head'
+import { PageMetaProps } from 'components/HeadMetadata'
 
 const pageMeta: PageMetaProps = {
   title: 'About',
-  description: '...',
 }
 
 export const getStaticProps: GetStaticProps = async () => {
