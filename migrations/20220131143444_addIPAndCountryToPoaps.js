05d29576454c39a34843ae1672a4a76a146ee166
Didier Krux <didier@krux.co>
2022-02-02 20:22:08 +0100

    autofix

--- migrations/20220131143444_addIPAndCountryToPoaps.js
+++ migrations/20220131143444_addIPAndCountryToPoaps.js
@@ -1,14 +1,14 @@
 const { TABLES } = require('../db')
 
 exports.up = async function (knex) {
-  await knex.schema.table(TABLES.poaps, table => {
+  await knex.schema.table(TABLES.poaps, (table) => {
     table.string('ip_address', 255).nullable().index()
     table.string('country_code', 255).nullable().index()
   })
 }
 
 exports.down = async function (knex) {
-  await knex.schema.table(TABLES.poaps, table => {
+  await knex.schema.table(TABLES.poaps, (table) => {
     table.dropColumn('ip_address')
     table.dropColumn('country_code')
   })
