f33dc56689786948fc7b7ab930056efc28dd42f2
Didier Krux <didier@krux.co>
2022-02-09 17:57:45 +0000

    HACK: manually add previous POAP events claims

--- src/components/LessonCards.tsx
+++ src/components/LessonCards.tsx
@@ -10,16 +10,23 @@ import LESSONS from 'constants/lessons'
 import LessonBanner from 'components/LessonBanner'
 
 const LessonCard = styled(Box)`
-  background: linear-gradient(
+  /* background: linear-gradient(
     152.97deg,
     rgba(0, 0, 0, 0.45) 0%,
     rgba(38, 38, 38, 0.25) 100%
-  );
+  ); */
   backdrop-filter: blur(42px);
 `
 
+// HACK: manually add previous POAP events claims
+// https://poap.gallery/event/6454 + https://poap.gallery/event/16394 = 55 + 330 = 385
+// https://poap.gallery/event/6455 + https://poap.gallery/event/21670 = 48 + 250 = 298
+const PREVIOUS_EVENTS_CLAIMED = [385, 298]
+
 const LessonCards: React.FC = () => {
-  const [numberOfPoapClaimed, setNumberOfPoapClaimed] = useState([])
+  const [numberOfPoapClaimed, setNumberOfPoapClaimed] = useState(
+    PREVIOUS_EVENTS_CLAIMED
+  )
 
   useEffect((): void => {
     // TODO: replace with tokensQuantityByEventId https://github.com/poap-xyz/poap-webapp/blob/2def482ffec93e6cbc4e3c5e5a18000805cc6c2b/src/api.ts#L1235
@@ -35,7 +42,10 @@ const LessonCards: React.FC = () => {
       .all(promiseArray)
       .then((results) => {
         setNumberOfPoapClaimed(
-          results.map((r) => r.data.data.event?.tokenCount || 0)
+          results.map(
+            (r, i) =>
+              (r.data.data.event?.tokenCount || 0) + numberOfPoapClaimed[i]
+          )
         )
       })
       .catch(function (error) {
