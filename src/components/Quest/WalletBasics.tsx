d3b8cfb5a65293177cad94f5f7547a04b70d93f0
Didier Krux <didier@krux.co>
2022-02-10 17:19:34 +0000

    improve height in wallet basics quest

--- src/components/Quest/WalletBasics.tsx
+++ src/components/Quest/WalletBasics.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Spinner } from '@chakra-ui/react'
+import { Button, Spinner, Box } from '@chakra-ui/react'
 import { Checks } from 'phosphor-react'
 import { useActiveWeb3React } from 'hooks'
 
@@ -13,42 +13,48 @@ const WalletBasics = (): {
     isQuestCompleted: !!account,
     questComponent: (
       <>
-        <div>
-          <h2>How to setup a MetaMask wallet</h2>
-          <p>
-            For convenience and ease of access throughout this lesson, you will
-            need a MetaMask wallet. Next we will demonstrate how to set up a
-            MetaMask wallet.
-          </p>
-          <p>
-            Download the browser extension from the official website:&nbsp;
-            <a
-              href="https://metamask.io/download"
-              target="_blank"
-              rel="noreferrer"
+        <Box display="flex">
+          <div className="bloc1">
+            <h2>How to setup a MetaMask wallet</h2>
+            <p>
+              For convenience and ease of access throughout this lesson, you
+              will need a MetaMask wallet. Next we will demonstrate how to set
+              up a MetaMask wallet.
+            </p>
+            <p>
+              Download the browser extension from the official website:&nbsp;
+              <a
+                href="https://metamask.io/download"
+                target="_blank"
+                rel="noreferrer"
+              >
+                https://metamask.io/download
+              </a>
+            </p>
+            <Button
+              variant="outlined"
+              leftIcon={account ? <Checks /> : <Spinner speed="1s" />}
+              color={account ? 'rgb(68, 169, 145)' : 'orange'}
+              cursor="default"
+              boxShadow="none !important"
             >
-              https://metamask.io/download
-            </a>
-          </p>
-        </div>
-        <iframe
-          src="https://www.youtube.com/embed/PjBY0pVFnQ8?rel=0"
-          allowFullScreen
-        ></iframe>
-        <Button
-          variant="outlined"
-          leftIcon={account ? <Checks /> : <Spinner speed="1s" />}
-          color={account ? 'rgb(68, 169, 145)' : 'orange'}
-          cursor="default"
-          boxShadow="none !important"
-        >
+              {account
+                ? 'Congrats for connecting your wallet! 👏🙂'
+                : 'Waiting to detect your wallet ...'}
+            </Button>
+          </div>
+          <div className="bloc2">
+            <iframe
+              src="https://www.youtube.com/embed/PjBY0pVFnQ8?rel=0"
+              allowFullScreen
+            ></iframe>
+          </div>
+        </Box>
+        <p>
           {account
-            ? 'Congrats for connecting your wallet! 👏🙂'
-            : 'Waiting to detect your wallet ...'}
-        </Button>
-        {account
-          ? null
-          : 'To validate this quest and finish this lesson, connect your wallet to this website. To do this, click the "Connect wallet" button in the top-right corner.'}
+            ? null
+            : 'To validate this quest and finish this lesson, connect your wallet to this website. To do this, click the "Connect wallet" button in the top-right corner.'}
+        </p>
       </>
     ),
   }
