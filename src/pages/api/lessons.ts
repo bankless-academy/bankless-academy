52d7c810b69897108dc65781ab41511c8b8316b4
Didier Krux <didier@krux.co>
2022-02-14 23:43:03 +0000

    use absolute path for asset in API

--- src/pages/api/lessons.ts
+++ src/pages/api/lessons.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import LESSONS from 'constants/lessons'
+import { DOMAIN } from 'constants/index'
 
 export default async function handler(
   req: NextApiRequest,
@@ -9,6 +10,12 @@ export default async function handler(
     return res.json(
       LESSONS.map((lesson) => {
         delete lesson.imageLinks
+        lesson.slides.map((slide) => {
+          if (slide.content) {
+            slide.content = slide.content.replace(/src='\//g, `src='${DOMAIN}/`)
+          }
+          return slide
+        })
         return lesson
       })
     )
