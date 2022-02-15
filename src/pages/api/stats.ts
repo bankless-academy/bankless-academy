5f40576014d6d6ac44f91cb84b29066b11000898
Didier Krux <didier@krux.co>
2022-02-15 13:18:12 +0000

    update stat API

--- src/pages/api/stats.ts
+++ src/pages/api/stats.ts
@@ -2,13 +2,13 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
 import { db, TABLES } from 'utils/db'
-import { POAP_EVENT_IDS } from 'constants/index'
+import { POAP_EVENT_IDS, POAP_QUESTS } from 'constants/index'
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> {
-  const stats: any = {}
+  const stats = { uniqueAddresses: {}, remainingPoaps: {}, poapDistributed: {} }
   try {
     const [uniqueAddresses] = await db(TABLES.users).count('id')
     stats.uniqueAddresses = uniqueAddresses.count
@@ -18,14 +18,22 @@ export default async function handler(
       .where('is_claimed', false)
       .whereIn('event_id', POAP_EVENT_IDS)
       .groupBy('event_id')
-    stats.remainingPoaps = remainingPoaps
+      .orderBy('event_id')
+    for (const event of remainingPoaps) {
+      const slug = POAP_QUESTS[event.event_id]
+      if (slug) stats.remainingPoaps[slug] = event.remainingPoaps
+    }
     const poapDistributed = await db(TABLES.poaps)
       .count('id', { as: 'poapDistributed' })
       .distinct('event_id')
       .where('is_claimed', true)
       .whereIn('event_id', POAP_EVENT_IDS)
       .groupBy('event_id')
-    stats.poapDistributed = poapDistributed
+      .orderBy('event_id')
+    for (const event of poapDistributed) {
+      const slug = POAP_QUESTS[event.event_id]
+      if (slug) stats.poapDistributed[slug] = event.poapDistributed
+    }
     return res.json(stats)
   } catch (error) {
     console.error(error)
