474bd547a84d585f38922911034cd450ad206e93
Didier Krux <didier@krux.co>
2022-04-22 20:30:26 +0100

    fix umami script

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -18,7 +18,7 @@ const umamiWebsiteId =
   VERCEL_ENV === 'production'
     ? '62d1cf48-425d-4658-9b86-3eea78ac9714'
     : 'e84c3a1e-0ab0-4502-b0fe-67d660765535'
-const umamiDomain = 'https://stats.banklessacademy.com/umami.js'
+const umamiDomain = 'https://stats.banklessacademy.com/stats.js'
 
 const Head = ({ metadata }: { metadata: MetaData }): React.ReactElement => {
   const router = useRouter()
@@ -29,39 +29,46 @@ const Head = ({ metadata }: { metadata: MetaData }): React.ReactElement => {
   const image = metadata?.image || DEFAULT_METADATA.image
   const url = `${DOMAIN}${router.asPath}`
   return (
-    <NextHead>
-      <title>{title}</title>
-      <meta name="description" content={description} />
-      {/* Open Graph / Facebook (needs to be < 300kb to work on WhatsApp) */}
-      <meta property="og:type" content="website" />
-      <meta property="og:url" content={url} />
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
-      <meta property="og:image" content={image} />
-      <meta property="og:image:width" content="1200" />
-      <meta property="og:image:height" content="628" />
-      {/* Twitter */}
-      <meta property="twitter:url" content={url} />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={image} />
-      {/* Umami */}
-      <Script async defer data-website-id={umamiWebsiteId} src={umamiDomain} />
-      {/* Hotjar */}
-      {typeof window !== 'undefined' &&
-      window.location.hostname === 'app.banklessacademy.com'
-        ? hotjar.initialize(2568813, 6)
-        : null}
-      {/* shortcut icon */}
-      <link
-        rel="shortcut icon"
-        sizes="200x200"
-        type="image/png"
-        href="/favicon.png"
-      />
-      <noscript>You need to enable JavaScript to run this app.</noscript>
-    </NextHead>
+    <>
+      <NextHead>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        {/* Open Graph / Facebook (needs to be < 300kb to work on WhatsApp) */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={image} />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="628" />
+        {/* Twitter */}
+        <meta property="twitter:url" content={url} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content={image} />
+        {/* Umami */}
+        {/* Hotjar */}
+        {typeof window !== 'undefined' &&
+        window.location.hostname === 'app.banklessacademy.com'
+          ? hotjar.initialize(2568813, 6)
+          : null}
+        {/* shortcut icon */}
+        <link
+          rel="shortcut icon"
+          sizes="200x200"
+          type="image/png"
+          href="/favicon.png"
+        />
+        <noscript>You need to enable JavaScript to run this app.</noscript>
+      </NextHead>
+      <Script
+        async
+        defer
+        data-website-id={umamiWebsiteId}
+        src={umamiDomain}
+      ></Script>
+    </>
   )
 }
 
