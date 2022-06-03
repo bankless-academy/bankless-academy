95770a99e33c678454e5d0031516541d4f36356a
Didier Krux <didier@krux.co>
2022-06-03 21:04:03 +0100

    filter lesson by publicationStatus

--- src/components/LessonCards.tsx
+++ src/components/LessonCards.tsx
@@ -35,58 +35,60 @@ const LessonCards: React.FC = () => {
 
   return (
     <>
-      {LESSONS.map((lesson, index) => {
-        const [isSmallScreen] = useMediaQuery('(max-width: 800px)')
-        // lesson not started yet: -1
-        // const currentSlide = parseInt(localStorage.getItem(lesson.slug) || '-1')
-        // const numberOfSlides = lesson.slides.length
-        const isPoapClaimed = localStorage.getItem(`poap-${lesson.slug}`)
-        const lessonCompleted =
-          (lesson.quest && stats?.lessonCompleted[lesson.quest]) || 0
-        return (
-          <LessonCard
-            key={`lesson-${index}`}
-            p={4}
-            pb={8}
-            borderBottom={
-              isSmallScreen && index + 1 < LESSONS.length
-                ? '1px solid #72757b'
-                : ''
-            }
-          >
-            <Text fontSize="xl" fontWeight="bold">
-              {lesson.name}
-            </Text>
-            <Text fontSize="lg">{lesson.description}</Text>
-            <Box display="flex" justifyContent="space-between" my="4">
-              <Tag size="sm" variant={isPoapClaimed ? 'solid' : 'outline'}>
-                {isPoapClaimed ? 'Done' : `${lesson.duration} minutes`}
-                {isPoapClaimed ? (
-                  <TagRightIcon as={CircleWavyCheck} weight="bold" />
-                ) : null}
-              </Tag>
-              <Text fontSize="sm">
-                {lessonCompleted > 0 && `${lessonCompleted} Completions`}
+      {LESSONS.filter((lesson) => lesson.publicationStatus === 'publish').map(
+        (lesson, index) => {
+          const [isSmallScreen] = useMediaQuery('(max-width: 800px)')
+          // lesson not started yet: -1
+          // const currentSlide = parseInt(localStorage.getItem(lesson.slug) || '-1')
+          // const numberOfSlides = lesson.slides.length
+          const isPoapClaimed = localStorage.getItem(`poap-${lesson.slug}`)
+          const lessonCompleted =
+            (lesson.quest && stats?.lessonCompleted[lesson.quest]) || 0
+          return (
+            <LessonCard
+              key={`lesson-${index}`}
+              p={4}
+              pb={8}
+              borderBottom={
+                isSmallScreen && index + 1 < LESSONS.length
+                  ? '1px solid #72757b'
+                  : ''
+              }
+            >
+              <Text fontSize="xl" fontWeight="bold">
+                {lesson.name}
               </Text>
-            </Box>
-            <Link href={`/lessons/${lesson.slug}`}>
-              <LessonBanner
-                cursor="pointer"
-                // overflow="hidden"
-                style={{
-                  aspectRatio: '1.91/1',
-                }}
-                py="4"
-              >
-                <Image src={lesson.lessonImageLink} />
-              </LessonBanner>
-            </Link>
-            <Text fontSize="md" mt="4">
-              {ReactHtmlParser(lesson.learnings)}
-            </Text>
-          </LessonCard>
-        )
-      })}
+              <Text fontSize="lg">{lesson.description}</Text>
+              <Box display="flex" justifyContent="space-between" my="4">
+                <Tag size="sm" variant={isPoapClaimed ? 'solid' : 'outline'}>
+                  {isPoapClaimed ? 'Done' : `${lesson.duration} minutes`}
+                  {isPoapClaimed ? (
+                    <TagRightIcon as={CircleWavyCheck} weight="bold" />
+                  ) : null}
+                </Tag>
+                <Text fontSize="sm">
+                  {lessonCompleted > 0 && `${lessonCompleted} Completions`}
+                </Text>
+              </Box>
+              <Link href={`/lessons/${lesson.slug}`}>
+                <LessonBanner
+                  cursor="pointer"
+                  // overflow="hidden"
+                  style={{
+                    aspectRatio: '1.91/1',
+                  }}
+                  py="4"
+                >
+                  <Image src={lesson.lessonImageLink} />
+                </LessonBanner>
+              </Link>
+              <Text fontSize="md" mt="4">
+                {ReactHtmlParser(lesson.learnings)}
+              </Text>
+            </LessonCard>
+          )
+        }
+      )}
     </>
   )
 }
