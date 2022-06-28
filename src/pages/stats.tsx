cc3bb5eb88ad73860c34559cb89fea25eaa05c51
Didier Krux <didier@krux.co>
2022-06-28 12:31:36 +0100

    hide POAP stats for whitelabel

--- src/pages/stats.tsx
+++ src/pages/stats.tsx
@@ -4,6 +4,7 @@ import { GetStaticProps } from 'next'
 import axios from 'axios'
 
 import { MetaData } from 'components/Head'
+import { IS_WHITE_LABEL } from 'constants/'
 import LESSONS from 'constants/lessons'
 
 const pageMeta: MetaData = {
@@ -32,51 +33,55 @@ const Stats = (): JSX.Element => {
     <Container maxW="container.xl">
       {stats && (
         <Box maxW="500px">
-          <Box mt={8}>
-            <Text fontSize="lg" color="orange.300">
-              Number of unique addresses connected to the app
-            </Text>
-            {stats?.uniqueAddresses}
-          </Box>
-          <Box mt={8}>
-            <Text fontSize="lg" color="orange.300">
-              Remaining POAPs
-            </Text>
-            {LESSONS.map((lesson, index) => {
-              const remainingPoaps =
-                stats?.remainingPoaps[lesson.poapEventId] || 0
-              return (
-                <>
-                  <p>
-                    {`${index + 1}. ${lesson.name}: `}
-                    {remainingPoaps}
-                    <Progress
-                      value={(remainingPoaps / 200) * 100}
-                      size="md"
-                      colorScheme="yellow"
-                    />
-                  </p>
-                </>
-              )
-            })}
-          </Box>
-          <Box mt={8}>
-            <Text fontSize="lg" color="orange.300">
-              POAP codes distributed
-            </Text>
-            {LESSONS.map((lesson, index) => {
-              const poapDistributed =
-                stats?.poapDistributed[lesson.poapEventId] || 0
-              return (
-                <>
-                  <p>
-                    {`${index + 1}. ${lesson.name}: `}
-                    {poapDistributed}
-                  </p>
-                </>
-              )
-            })}
-          </Box>
+          {!IS_WHITE_LABEL && (
+            <>
+              <Box mt={8}>
+                <Text fontSize="lg" color="orange.300">
+                  Number of unique addresses connected to the app
+                </Text>
+                {stats?.uniqueAddresses}
+              </Box>
+              <Box mt={8}>
+                <Text fontSize="lg" color="orange.300">
+                  Remaining POAPs
+                </Text>
+                {LESSONS.map((lesson, index) => {
+                  const remainingPoaps =
+                    stats?.remainingPoaps[lesson.poapEventId] || 0
+                  return (
+                    <>
+                      <p>
+                        {`${index + 1}. ${lesson.name}: `}
+                        {remainingPoaps}
+                        <Progress
+                          value={(remainingPoaps / 200) * 100}
+                          size="md"
+                          colorScheme="yellow"
+                        />
+                      </p>
+                    </>
+                  )
+                })}
+              </Box>
+              <Box mt={8}>
+                <Text fontSize="lg" color="orange.300">
+                  POAP codes distributed
+                </Text>
+                {LESSONS.map((lesson, index) => {
+                  const poapDistributed =
+                    stats?.poapDistributed[lesson.poapEventId] || 0
+                  return (
+                    <>
+                      <p>
+                        {`${index + 1}. ${lesson.name}: `}
+                        {poapDistributed}
+                      </p>
+                    </>
+                  )
+                })}
+              </Box>
+            </>
+          )}
           <Box mt={8}>
             <Text fontSize="lg" color="orange.300">
               Number of lesson completion
