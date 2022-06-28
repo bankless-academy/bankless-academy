b9397ea8d90df4bb6f430b41366efc638788d0ed
Didier Krux <didier@krux.co>
2022-06-28 18:31:52 +0100

    update BanklessAcademyCommunity quest

--- src/components/Quest/BanklessAcademyCommunity.tsx
+++ src/components/Quest/BanklessAcademyCommunity.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { Box, VStack, Button, Spinner, Link } from '@chakra-ui/react'
+import { useMediaQuery } from '@chakra-ui/react'
 import { useRouter } from 'next/router'
 import { CheckIcon } from '@chakra-ui/icons'
 import axios from 'axios'
@@ -15,6 +16,7 @@ const BanklessAcademyCommunity = (): {
 
   const [hasUserJoinedGM, setHasUserJoinedGM] = useState(false)
   const [hasUserJoinedBA, setHasUserJoinedBA] = useState(false)
+  const [isSmallScreen] = useMediaQuery('(max-width: 800px)')
 
   useEffect(() => {
     verifyQuest()
@@ -54,73 +56,103 @@ const BanklessAcademyCommunity = (): {
     isQuestCompleted: account && hasUserJoinedGM && hasUserJoinedBA,
     questComponent: (
       <>
-        <Box>
-          <VStack>
-            <Button
-              cursor="default"
-              rightIcon={
-                account ? (
-                  <CheckIcon color="green.500" />
-                ) : (
-                  <Spinner speed="1s" />
-                )
-              }
-            >
-              {'1. Connect your wallet to Bankless Academy'}
-            </Button>
-            <Button
-              cursor="default"
-              rightIcon={
-                hasUserJoinedGM ? (
-                  <CheckIcon color="green.500" />
-                ) : (
-                  <Spinner speed="1s" />
-                )
-              }
-            >
-              {'2. Join '}
-              <Link
-                href="https://gm.xyz/?utm_source=BanklessAcademy&utm_medium=website&utm_campaign=GM-lesson"
-                target="_blank"
-                ml="1"
-              >
-                gm.xyz
-              </Link>
-            </Button>
-            <Button
-              cursor="default"
-              rightIcon={
-                hasUserJoinedBA ? (
-                  <CheckIcon color="green.500" />
-                ) : (
-                  <Spinner speed="1s" />
-                )
-              }
-            >
-              {'3. Join '}
-              <Link
-                href="https://gm.xyz/c/BanklessAcademy?utm_source=BanklessAcademy&utm_medium=website&utm_campaign=GM-lesson"
-                target="_blank"
-                ml="1"
-              >
-                gm.xyz/c/BanklessAcademy
-              </Link>
-            </Button>
-          </VStack>
-          <Box mt="16">
-            <Button onClick={() => router.push('/lessons')} variant="outline">
-              Skip quest
-            </Button>
-            <Button
-              colorScheme={hasUserJoinedGM && hasUserJoinedBA ? 'green' : 'red'}
-              onClick={verifyQuest}
-              variant="primary"
-              ml="8"
-            >
-              Validate quest
-            </Button>
+        {!account && (
+          <Button
+            variant="outlined"
+            leftIcon={<Spinner speed="1s" />}
+            color={'orange'}
+            cursor="default"
+            boxShadow="none !important"
+          >
+            Waiting to detect your wallet ...
+          </Button>
+        )}
+        {account ? (
+          <Box display={isSmallScreen ? 'block' : 'flex'}>
+            <div className="bloc1">
+              <Box>
+                <VStack mt="20">
+                  <Button
+                    cursor="default"
+                    rightIcon={
+                      account ? (
+                        <CheckIcon color="green.500" />
+                      ) : (
+                        <Spinner speed="1s" />
+                      )
+                    }
+                  >
+                    {'1. Connect your wallet to Bankless Academy'}
+                  </Button>
+                  <Button
+                    cursor="default"
+                    rightIcon={
+                      hasUserJoinedGM ? (
+                        <CheckIcon color="green.500" />
+                      ) : (
+                        <Spinner speed="1s" />
+                      )
+                    }
+                  >
+                    {'2. Join '}
+                    <Link
+                      href="https://gm.xyz/?utm_source=BanklessAcademy&utm_medium=website&utm_campaign=GM-lesson"
+                      target="_blank"
+                      ml="1"
+                    >
+                      gm.xyz
+                    </Link>
+                  </Button>
+                  <Button
+                    cursor="default"
+                    rightIcon={
+                      hasUserJoinedBA ? (
+                        <CheckIcon color="green.500" />
+                      ) : (
+                        <Spinner speed="1s" />
+                      )
+                    }
+                  >
+                    {'3. Join '}
+                    <Link
+                      href="https://gm.xyz/c/BanklessAcademy?utm_source=BanklessAcademy&utm_medium=website&utm_campaign=GM-lesson"
+                      target="_blank"
+                      ml="1"
+                    >
+                      gm.xyz/c/BanklessAcademy
+                    </Link>
+                  </Button>
+                </VStack>
+                <Box mt="16" textAlign="center">
+                  <Button
+                    onClick={() => router.push('/lessons')}
+                    variant="outline"
+                  >
+                    Skip quest
+                  </Button>
+                  <Button
+                    colorScheme={
+                      hasUserJoinedGM && hasUserJoinedBA ? 'green' : 'red'
+                    }
+                    onClick={verifyQuest}
+                    variant="primary"
+                    ml="8"
+                  >
+                    Validate quest
+                  </Button>
+                </Box>
+              </Box>
+            </div>
+            <div className="bloc2">
+              <iframe
+                src="https://www.youtube.com/embed/ZPost3rrISQ?rel=0"
+                allowFullScreen
+              ></iframe>
+            </div>
           </Box>
-        </Box>
+        ) : (
+          'To validate this quest and finish this lesson, connect your wallet to this website. To do this, click the "Connect wallet" button in the top-right corner.'
+        )}
       </>
     ),
   }
