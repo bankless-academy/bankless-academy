68c73e953b0908c1b4c844dc75382bb3d00982fb
Didier Krux <didier@krux.co>
2022-07-28 21:35:31 +0200

    add FeaturedLessons component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,13 +12,12 @@ import {
   Link,
 } from '@chakra-ui/react'
 import NextLink from 'next/link'
-import styled from '@emotion/styled'
 
 import Footer from 'layout/Footer'
-import LessonBanner from 'components/LessonBanner'
+import FeaturedLessons from 'components/FeaturedLessons'
 import WhitelabelHomepage from 'pages/whitelabel_homepage'
 import { LearnIcon, QuizIcon, QuestIcon, PoapIcon } from 'components/Icons'
-import { LESSONS, HOMEPAGE_BACKGROUND, IS_WHITELABEL } from 'constants/index'
+import { HOMEPAGE_BACKGROUND, IS_WHITELABEL } from 'constants/index'
 
 const MORE_LESSONS = [
   'Next Level: Intermediate Wallet',
@@ -30,13 +29,6 @@ const MORE_LESSONS = [
   'DeFi Skills: Alchemix Basics',
   'DeFi Skills: Working with Layer 2s',
 ]
-
-const LessonGrid = styled(SimpleGrid)`
-  border-bottom: 1px solid #72757b;
-  :last-child {
-    border-bottom: none;
-  }
-`
 
 const HomePage = (): JSX.Element => {
   const [isSmallScreen] = useMediaQuery('(max-width: 800px)')
@@ -138,75 +130,7 @@ const HomePage = (): JSX.Element => {
                 </Box>
               </SimpleGrid>
             </Box>
-            <Box mt="16">
-              <Heading as="h2" size="xl">
-                Available Lessons
-              </Heading>
-              <Box>
-                {LESSONS.filter((lesson) => lesson.isFeaturedOnHomepage).map(
-                  (lesson, key) => {
-                    const isPoapClaimed = localStorage.getItem(
-                      `poap-${lesson.slug}`
-                    )
-                    const isLessonStarted =
-                      (localStorage.getItem(lesson.slug) || 0) > 0
-                    const LessonImage = (
-                      <LessonBanner
-                        iswhitelabel={IS_WHITELABEL}
-                        cursor="pointer"
-                        // overflow="hidden"
-                        style={{
-                          aspectRatio: '1.91/1',
-                        }}
-                        maxW="600px"
-                      >
-                        <NextLink href={`/lessons/${lesson.slug}`}>
-                          <Image src={lesson.lessonImageLink} />
-                        </NextLink>
-                      </LessonBanner>
-                    )
-                    const LessonDescription = (
-                      <Box alignSelf="center" mt="4">
-                        <Heading fontSize="2xl">{lesson.name}</Heading>
-                        <Text fontSize="lg" my="4">
-                          {lesson.marketingDescription}
-                        </Text>
-                        <NextLink href={`/lessons/${lesson.slug}`}>
-                          <Button variant="primary" mt="4">
-                            {isPoapClaimed
-                              ? 'Review Lesson'
-                              : isLessonStarted
-                              ? 'Resume Lesson'
-                              : 'Start Lesson'}
-                          </Button>
-                        </NextLink>
-                      </Box>
-                    )
-                    return (
-                      <LessonGrid
-                        columns={{ sm: 1, md: 2, lg: 2 }}
-                        key={key}
-                        gap={6}
-                        py="10"
-                        mx={isSmallScreen ? '0' : '12'}
-                      >
-                        {key % 2 === 0 || isSmallScreen ? (
-                          <>
-                            {LessonImage}
-                            {LessonDescription}
-                          </>
-                        ) : (
-                          <>
-                            {LessonDescription}
-                            {LessonImage}
-                          </>
-                        )}
-                      </LessonGrid>
-                    )
-                  }
-                )}
-              </Box>
-            </Box>
+            <FeaturedLessons />
             <>
               <Box mt="16">
                 <Heading as="h2" size="xl" mt="16" mb="8">
