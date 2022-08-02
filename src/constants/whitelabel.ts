2be6b8e6e95052b8796ad39486478c07af2d1621
Didier Krux <didier@krux.co>
2022-08-02 16:26:14 +0200

    fix typo

--- src/constants/whitelabel.ts
+++ src/constants/whitelabel.ts
@@ -3,7 +3,7 @@ import { WhitelabelType } from 'entities/whitelabel'
 export const WHITELABEL: WhitelabelType = {
   project_name: 'BanklessDAO Academy',
   domain_prod: 'academy.bankless.community',
-  default_metadata_description: 'Level up you Bankless DAO skills',
+  default_metadata_description: 'Level up your Bankless skills',
   default_metadata_image: '/BanklessDAO/image-f7b3de29.jpg',
   favicon: '/BanklessDAO/favicon-8166eefe.png',
   logo: '/BanklessDAO/logo-7ee3f640.png',
