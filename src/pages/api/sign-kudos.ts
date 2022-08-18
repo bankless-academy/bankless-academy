35961f683f3d0ca8f27240916d2dbc62bfa8d948
Didier Krux <didier@krux.co>
2022-08-18 12:22:21 +0100

    add kudos signature to DB

---
+++ src/pages/api/sign-kudos.ts
@@ -0,0 +1,74 @@
+/* eslint-disable no-console */
+import { NextApiRequest, NextApiResponse } from 'next'
+
+import { verifyTypedSignature } from 'utils'
+import { MINTKUDOS_DOMAIN_INFO } from 'constants/index'
+
+import { db, TABLES } from 'utils/db'
+import { GENERIC_ERROR_MESSAGE, LESSONS } from 'constants/index'
+
+const ALLOWED_SIGNERS = [
+  '0xbd19a3f0a9cace18513a1e2863d648d13975cb30'.toLowerCase(),
+  '0x181D10bd80c0F0f4f9959510Ce3F1c3A68BD783d'.toLowerCase(),
+]
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<void> {
+  // check params + signature
+  const { address, kudosId, signature } = req.body
+  console.log(req.body)
+  if (
+    !address ||
+    !kudosId ||
+    !signature ||
+    typeof address === 'object' ||
+    typeof signature === 'object'
+  )
+    return res.json({ error: 'Wrong params' })
+
+  console.log('address: ', address)
+  console.log('kudosId: ', kudosId)
+  console.log('signature: ', signature)
+
+  // TODO: require to sign in less than 2 minutes?
+
+  try {
+    if (!ALLOWED_SIGNERS.includes(address.toLowerCase()))
+      return res.json({ error: 'Address not allowed to sign' })
+
+    const types = {
+      AllowlistedAddress: [{ name: 'tokenId', type: 'uint256' }],
+    }
+    const message = {
+      tokenId: kudosId,
+    }
+    console.log('message', JSON.stringify(message))
+    if (
+      !verifyTypedSignature(
+        signature,
+        message,
+        address,
+        types,
+        MINTKUDOS_DOMAIN_INFO
+      )
+    )
+      return res.json({ error: 'Wrong signature' })
+
+    const notionId = LESSONS.find((lesson) => lesson.kudosId).notionId
+    if (!notionId) return res.json({ error: 'notionId not found' })
+
+    const updated = await db(TABLES.credentials)
+      .where('notion_id', notionId)
+      .update({ signature })
+    console.log(updated)
+    if (updated === 1) return res.json({ result: 'Signature updated' })
+    else return res.json({ error: 'notionID missing?' })
+  } catch (error) {
+    console.error(error)
+    res.json({
+      error: `error ${error?.code}: ${GENERIC_ERROR_MESSAGE}`,
+    })
+  }
+}
--- src/pages/kudos.tsx
+++ src/pages/kudos.tsx
@@ -2,10 +2,15 @@
 import React from 'react'
 import { Container, Button, Box, useToast } from '@chakra-ui/react'
 import { GetStaticProps } from 'next'
+import axios from 'axios'
 
 import { MetaData } from 'components/Head'
 import { useActiveWeb3React } from 'hooks'
-import { LESSONS } from 'constants/index'
+import {
+  LESSONS,
+  MINTKUDOS_CHAIN_ID,
+  MINTKUDOS_DOMAIN_INFO,
+} from 'constants/index'
 import { NETWORKS } from 'constants/networks'
 
 const pageMeta: MetaData = {
@@ -17,11 +22,6 @@ export const getStaticProps: GetStaticProps = async () => {
     props: { pageMeta },
   }
 }
-
-const MINTKUDOS_API = process.env.NEXT_PUBLIC_MINTKUDOS_API
-const IS_MINTKUDOS_SANDBOX =
-  MINTKUDOS_API === 'https://sandbox-api.mintkudos.xyz'
-const CHAIN_ID = IS_MINTKUDOS_SANDBOX ? 80001 : 137
 
 const Kudos = (): JSX.Element => {
   const { account, library, chainId } = useActiveWeb3React()
@@ -36,19 +36,10 @@ const Kudos = (): JSX.Element => {
     const value = {
       tokenId: kudosId,
     }
-    const domainInfo = {
-      name: 'Kudos',
-      version: '7',
-      // Mumbai | Polygon
-      chainId: CHAIN_ID,
-      verifyingContract: IS_MINTKUDOS_SANDBOX
-        ? '0xB876baF8F69cD35fb96A17a599b070FBdD18A6a1'
-        : '0x60576A64851C5B42e8c57E3E4A5cF3CF4eEb2ED6',
-    }
 
-    if (chainId !== CHAIN_ID) {
+    if (chainId !== MINTKUDOS_CHAIN_ID) {
       const network = Object.values(NETWORKS).find(
-        (network) => network.chainId === CHAIN_ID
+        (network) => network.chainId === MINTKUDOS_CHAIN_ID
       )
       toast.closeAll()
       toast({
@@ -60,16 +51,35 @@ const Kudos = (): JSX.Element => {
     } else {
       try {
         const signer = library.getSigner(account)
-        const signature = await signer._signTypedData(domainInfo, types, value)
+        const signature = await signer._signTypedData(
+          MINTKUDOS_DOMAIN_INFO,
+          types,
+          value
+        )
         console.log('signature', signature)
         toast.closeAll()
-        toast({
-          title: `Signature for ${kudosId}`,
-          description: signature,
-          status: 'success',
-          duration: null,
-        })
-        // TODO: save this in DB (verify signature + check if address is an authorized credential signer)
+        const bodyParameters = {
+          address: account,
+          kudosId,
+          signature,
+        }
+        const result = await axios.post(`/api/sign-kudos`, bodyParameters)
+        console.log(result.data)
+        if (result.data?.error) {
+          toast({
+            title: `Signature for ${kudosId}`,
+            description: result.data?.error,
+            status: 'error',
+            duration: 10000,
+          })
+        } else {
+          toast({
+            title: `Signature for ${kudosId}`,
+            description: result.data?.result,
+            status: 'success',
+            duration: 10000,
+          })
+        }
       } catch (error) {
         // TODO: add error feedback
         console.error(error)
