0cdfb9ab92316e238871e2d1d3c5790f7a90b2bc
Didier Krux <didier@krux.co>
2022-08-30 20:08:54 +0100

    comment localhost

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,7 +6,7 @@ export const DOMAIN_IDS = {
   'documentation.banklessacademy.com': '73cae3c0d9124d38babe1f1f9ec5c65f',
   'talent.banklessacademy.com': '208c77594ddc47ef9ea628c029d29ab0',
   'sponsors.banklessacademy.com': '56d3b0a011fe443aa2a9682f0ca443bb',
-  localhost: '8198f1db3f1c490cb2aedf361fc3e416',
+  // localhost: '8198f1db3f1c490cb2aedf361fc3e416',
 }
 
 export const ALLOWED_DOMAINS = Object.keys(DOMAIN_IDS)
@@ -16,6 +16,8 @@ export function middleware(request: NextRequest) {
     request?.nextUrl?.pathname === '/' &&
     ALLOWED_DOMAINS.includes(request?.nextUrl?.hostname)
   ) {
+    // eslint-disable-next-line no-console
+    console.log('nextUrl', request?.nextUrl)
     return NextResponse.rewrite(
       new URL(`/notion/${DOMAIN_IDS[request?.nextUrl?.hostname]}`, request.url)
     )
