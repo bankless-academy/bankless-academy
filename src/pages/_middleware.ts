893d5a3ec0e67cd563f61e457fe129b81aa4bf57
Didier Krux <didier@krux.co>
2022-08-30 19:25:11 +0100

    fix

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -4,12 +4,12 @@ import type { NextRequest } from 'next/server'
 // TODO: optimize https://nextjs.org/docs/messages/middleware-upgrade-guide
 
 export function middleware(request: NextRequest) {
+  // eslint-disable-next-line no-console
+  console.log('middle', request?.nextUrl)
   if (
     request?.nextUrl?.pathname === '/' &&
     request?.nextUrl?.hostname === 'whitelabel.banklessacademy.com'
   ) {
-    // eslint-disable-next-line no-console
-    console.log('middle', request?.nextUrl)
     return NextResponse.rewrite(
       new URL(
         '/notion/208c77594ddc47ef9ea628c029d29ab0',
