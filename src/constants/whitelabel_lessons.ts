6054a851130d3ca03a4c338da7702cde6ce18048
Didier Krux <didier@krux.co>
2022-09-19 13:43:54 +0100

    update BanklessDAO Constitution lesson

--- src/constants/whitelabel_lessons.ts
+++ src/constants/whitelabel_lessons.ts
@@ -24,10 +24,10 @@ const LESSONS: LessonType[] = [
     slug: 'banklessdao-constitution',
     imageLinks: [
       '/BanklessDAO/lesson/banklessdao-constitution/introduction-5d6722de.png',
-      '/BanklessDAO/lesson/banklessdao-constitution/the-genesis-def3d2c2.png',
-      '/BanklessDAO/lesson/banklessdao-constitution/the-bankless-movement-50f818c9.png',
+      '/BanklessDAO/lesson/banklessdao-constitution/the-genesis-5e72db39.png',
+      '/BanklessDAO/lesson/banklessdao-constitution/the-bankless-movement-8d542290.png',
       '/BanklessDAO/lesson/banklessdao-constitution/banklessdao-de8e4e7d.png',
-      '/BanklessDAO/lesson/banklessdao-constitution/mission-vision-05bf5e75.png',
+      '/BanklessDAO/lesson/banklessdao-constitution/mission-and-vision-bc729c04.png',
       '/BanklessDAO/lesson/banklessdao-constitution/values-1668d0af.png',
       '/BanklessDAO/lesson/banklessdao-constitution/values-520ec8cc.png',
       '/BanklessDAO/lesson/banklessdao-constitution/values-851d8d30.png',
@@ -37,7 +37,7 @@ const LESSONS: LessonType[] = [
       '/BanklessDAO/lesson/banklessdao-constitution/membership-levels-fe38ae52.png',
       '/BanklessDAO/lesson/banklessdao-constitution/membership-levels-467a5425.png',
       '/BanklessDAO/lesson/banklessdao-constitution/guilds-f9611a05.png',
-      '/BanklessDAO/lesson/banklessdao-constitution/projects-9b285c8a.png',
+      '/BanklessDAO/lesson/banklessdao-constitution/projects-62528474.png',
       '/BanklessDAO/lesson/banklessdao-constitution/grants-committee-ecfd569f.png',
       '/BanklessDAO/lesson/banklessdao-constitution/dao-cycles-79eed215.png',
       '/BanklessDAO/lesson/banklessdao-constitution/3-cycles-0f3079de.png',
@@ -56,19 +56,19 @@ const LESSONS: LessonType[] = [
         type: 'LEARN',
         notionId: 'f105296784134279a3d90697b6c0ace2',
         title: 'Introduction',
-        content: '<div class="bloc1"><p>The BanklessDAO Constitution and Community Handbook represent our community\'s values, mission, structure, processes, and governance. It is the codification of our community operating system and is meant to serve rather than constrain its members. Like our community itself, it will always be a work in progress. This lesson is a summary expression of that handbook. Please reference it directly as the canonical source for questions of process and procedure…</p></div><div class="bloc2"><img src=\'/BanklessDAO/lesson/banklessdao-constitution/introduction-5d6722de.png\'></div>'
+        content: '<div class="bloc1"><p>The BanklessDAO Constitution and Community Handbook captures our community\'s values, mission, structure, processes, and governance. It is the codification of our community operating system, meant to serve rather than constrain its members. Just like our community, it will always be a work in progress. This lesson is a summary of that Handbook. Please refer to the Handbook as the canonical source to answer questions of process and procedure.</p></div><div class="bloc2"><img src=\'/BanklessDAO/lesson/banklessdao-constitution/introduction-5d6722de.png\'></div>'
       },
       {
         type: 'LEARN',
         notionId: '110f75fb1df24b96b6f91758515ec899',
         title: 'The Genesis',
-        content: '<div class="bloc1"><p>BanklessDAO was founded by David Hoffman and Ryan Sean Adams on May 4th, 2021, when they airdropped BANK tokens to each Bankless premium subscriber. Following the true ethos of decentralization, they stepped back and let the DAO take shape. While their media company, Bankless LLC, continues to operate as its own media node, the Bankless brand is propagated through the DAO and its sub-DAOs. Now, the Bankless ecosystem as a whole creates user-friendly onramps for people to discover decentralized financial technologies through education, media, and culture.</p></div><div class="bloc2"><img src=\'/BanklessDAO/lesson/banklessdao-constitution/the-genesis-def3d2c2.png\'></div>'
+        content: '<div class="bloc1"><p>BanklessDAO was founded by David Hoffman and Ryan Sean Adams on May 4, 2021, when they airdropped BANK tokens to each Bankless newsletter premium subscriber (see <a href=\'https://snapshot.org/#/banklessvault.eth/proposal/QmdoixPMMT76vSt6ewkE87JZJywS1piYsGC3nJJpcrPXKS\'>The Genesis Proposal</a>). Following the true ethos of decentralization, they stepped back and let the DAO take shape. While their media company, Bankless LLC, continues to operate as its own media node, the Bankless brand is further propagated through the DAO and its sub-DAOs. Today, the Bankless ecosystem creates user-friendly onramps for people to discover decentralized financial technologies through education, media, and culture.</p></div><div class="bloc2"><img src=\'/BanklessDAO/lesson/banklessdao-constitution/the-genesis-5e72db39.png\'></div>'
       },
       {
         type: 'LEARN',
         notionId: '9669e4350d404026b190a327308c62bf',
         title: 'The Bankless Movement',
-        content: '<div class="bloc1"><p>Bankless is a movement for pioneers seeking liberation from the tyranny of the traditional financial system. Going Bankless means adopting decentralized, permissionless, and censorship-resistant technology. We will achieve financial self-sovereignty, security, and prosperity through these means.</p><p>We seek new ways to build wealth, solve human coordination problems, create culture, and develop cutting-edge thought-leadership in the crypto space. Going Bankless means taking control of your financial health by adopting decentralized and permissionless money systems.</p><p>Bankless is the movement educating the world on how to do it.</p></div><div class="bloc2"><img src=\'/BanklessDAO/lesson/banklessdao-constitution/the-bankless-movement-50f818c9.png\'></div>'
+        content: '<div class="bloc1"><p>Bankless is a movement for pioneers seeking liberation from the tyranny of the traditional financial system. Going Bankless means adopting decentralized, permissionless, and censorship-resistant technology to enable financial self-sovereignty, security, and prosperity.</p><p>We seek new ways to build wealth, solve human coordination problems, create culture, and develop cutting-edge thought leadership in the crypto space. Going Bankless means taking control of your financial health by adopting decentralized and permissionless money systems.</p></div><div class="bloc2"><img src=\'/BanklessDAO/lesson/banklessdao-constitution/the-bankless-movement-8d542290.png\'></div>'
       },
       {
         type: 'QUIZ',
@@ -90,19 +90,19 @@ const LESSONS: LessonType[] = [
         type: 'LEARN',
         notionId: '45ad4dde19464097bebc4ad355293d52',
         title: 'BanklessDAO',
-        content: '<div class="bloc1"><p>The BanklessDAO is a decentralized autonomous organization that acts as a steward of the Bankless Movement, progressing the world towards a future of greater freedom. BanklessDAO is a community of passionate Web3 thought-leaders who aim to educate new users, produce written and audio media, build DAO tooling, provide consulting services, develop Web3 scholarship, and create a social and creative home base for anyone in Web3. We’re so much more than a social DAO—we’re a shipping, building, and innovating DAO. See <a href=\'https://snapshot.org/#/banklessvault.eth/proposal/QmdoixPMMT76vSt6ewkE87JZJywS1piYsGC3nJJpcrPXKS\'>The Genesis Proposal</a></p></div><div class="bloc2"><img src=\'/BanklessDAO/lesson/banklessdao-constitution/banklessdao-de8e4e7d.png\'></div>'
+        content: '<div class="bloc1"><p>BanklessDAO is a decentralized autonomous organization that stewards the Bankless movement, progressing the world towards a future of greater freedom. BanklessDAO is a community of passionate Web3 thought leaders who aim to educate new users, produce media content, build DAO tooling, provide consulting services, develop Web3 scholarship, and create a social and creative home base for anyone in Web3. We’re so much more than a social DAO — we’re a shipping, building, and innovating DAO. </p></div><div class="bloc2"><img src=\'/BanklessDAO/lesson/banklessdao-constitution/banklessdao-de8e4e7d.png\'></div>'
       },
       {
         type: 'LEARN',
         notionId: 'd6e6c64700634fef8bbb9c9dc832778f',
-        title: 'Mission & Vision',
-        content: '<div class="bloc1"><p>Our mission is to help the world go Bankless by creating user-friendly onramps for people to discover decentralized financial technologies through education, media, and culture. The nodes in our growing ecosystem collaborate to create inroads for people to discover, use, learn and enjoy the benefits of Web3. Everyone falls down the rabbit hole in a different way—we provide those diverse paths.</p><p>Our Vision is to make financial independence, decentralized work, and creative freedom accessible to all. We work to build a world where anyone with an internet connection has access to the tools needed to achieve financial independence. We want everyone to be able to open a computer, find a DAO, start working, and get paid. We want to enable visionaries to unlock their true creative freedom with the power of Web3.</p></div><div class="bloc2"><img src=\'/BanklessDAO/lesson/banklessdao-constitution/mission-vision-05bf5e75.png\'></div>'
+        title: 'Mission and Vision',
+        content: '<div class="bloc1"><p>Our mission is to help the world go Bankless by creating user-friendly onramps for people to discover decentralized financial technologies through education, media, and culture. The nodes in our growing ecosystem collaborate to create inroads for people to discover, use, learn, and enjoy the benefits of Web3. Everyone finds their way to the rabbit hole differently — we provide those diverse paths.</p><p>Our vision is to make financial independence, decentralized work, and creative freedom accessible to all. We work to build a world where anyone with an internet connection has access to the tools needed to achieve financial independence. We want everyone to be able to get online, find a DAO, start working, and get paid. This is how we enable visionaries to unlock their true creative freedom with the power of Web3.</p></div><div class="bloc2"><img src=\'/BanklessDAO/lesson/banklessdao-constitution/mission-and-vision-bc729c04.png\'></div>'
       },
       {
         type: 'LEARN',
         notionId: '3d45ea8b6eb2413eb41a556edc4174dc',
         title: 'Values',
-        content: '<div class="bloc-ab"><div class="bloc-a"><img src=\'/BanklessDAO/lesson/banklessdao-constitution/values-1668d0af.png\'></div><div class="bloc-b"><h2>Education</h2><p>We seek to become a trusted guide that empowers people all over the globe to adopt decentralized finance by sharing accurate, truthful, and objective information.</p></div></div><div class="bloc-ab"><div class="bloc-a"><img src=\'/BanklessDAO/lesson/banklessdao-constitution/values-520ec8cc.png\'></div><div class="bloc-b"><h2>Integrity</h2><p>We operate transparently and build trust through radically public discourse and financial auditability.</p></div></div><div class="bloc-ab"><div class="bloc-a"><img src=\'/BanklessDAO/lesson/banklessdao-constitution/values-851d8d30.png\'></div><div class="bloc-b"><h2>Decentralized Governance</h2><p>We put decision-making into the hands of the collective. We create legitimacy through an environment where the best ideas win.</p></div></div><div class="bloc-ab"><div class="bloc-a"><img src=\'/BanklessDAO/lesson/banklessdao-constitution/values-2be7d002.png\'></div><div class="bloc-b"><h2>Culture</h2><p>We reward action and embrace risk. We empower our community to continually drive new initiatives by providing a space to self-organize and quickly move from idea to action.</p></div></div>'
+        content: '<div class="bloc-ab"><div class="bloc-a"><img src=\'/BanklessDAO/lesson/banklessdao-constitution/values-1668d0af.png\'></div><div class="bloc-b"><h2>Education</h2><p>By sharing accurate, truthful, and objective information, we seek to become a trusted guide that empowers people all over the globe to adopt decentralized finance.</p></div></div><div class="bloc-ab"><div class="bloc-a"><img src=\'/BanklessDAO/lesson/banklessdao-constitution/values-520ec8cc.png\'></div><div class="bloc-b"><h2>Integrity</h2><p>We operate transparently and build trust through radically public discourse and financial auditability.</p></div></div><div class="bloc-ab"><div class="bloc-a"><img src=\'/BanklessDAO/lesson/banklessdao-constitution/values-851d8d30.png\'></div><div class="bloc-b"><h2>Decentralized Governance</h2><p>We put decision making into the hands of the collective. We create legitimacy through an environment where the best ideas win.</p></div></div><div class="bloc-ab"><div class="bloc-a"><img src=\'/BanklessDAO/lesson/banklessdao-constitution/values-2be7d002.png\'></div><div class="bloc-b"><h2>Culture</h2><p>We reward action and embrace risk. We empower our community to continually drive new initiatives by providing a space to self organize and quickly move from idea to action.</p></div></div>'
       },
       {
         type: 'QUIZ',
@@ -124,26 +124,26 @@ const LESSONS: LessonType[] = [
         type: 'LEARN',
         notionId: '004bcf07e3f94a22a4f2cd0b9eebb382',
         title: 'DAO Membership',
-        content: '<div class="bloc1"><p>The collective body of BanklessDAO Discord users is referred to as the BanklessDAO community. Membership levels are determined using community-wide roles in Discord. While these roles may grant owners specific access and admin privileges, they do not constitute a hierarchical system granting authority over others. They are social signals designed to aid in communication and coordination. </p><p>All non-members can get a free 14-day guest pass by completing our onboarding process to test the community waters. Individuals who desire to move beyond the guest status to full membership (L1) do so by authenticating their possession of the requisite 35k BANK in Discord. There are 3 levels beyond L1, which we will look at next. </p></div><div class="bloc2"><img src=\'/BanklessDAO/lesson/banklessdao-constitution/dao-membership-7caabebd.png\'></div>'
+        content: '<div class="bloc1"><p>BanklessDAO Discord users are collectively referred to as the BanklessDAO community. Membership levels correspond with community-wide roles in Discord. While these roles may grant owners specific access and admin privileges, they do not constitute a hierarchical system granting authority over others. Instead, these roles are social signals designed to aid in communication and coordination. </p><p>All non-members can obtain a 14-day guest pass upon completing our onboarding process. Individuals who desire to move beyond the guest status to full membership (L1) can do so by earning or purchasing the required 35K BANK, and then verifying their balance in Discord. There are three levels beyond L1, which we will look at next. </p></div><div class="bloc2"><img src=\'/BanklessDAO/lesson/banklessdao-constitution/dao-membership-7caabebd.png\'></div>'
       },
       {
         type: 'LEARN',
         notionId: 'e7a7a8105f6f43beaa619545533dd3ff',
         title: 'Membership Levels',
-        content: '<div class="bloc-ab"><div class="bloc-a"><img src=\'/BanklessDAO/lesson/banklessdao-constitution/membership-levels-b13f5565.png\'></div><div class="bloc-b"><h2><strong>Contributors (L2)</strong></h2><p>Contributor status is achieved via nomination from one\'s peers as valuable members of the DAO and are expected to contribute to the overall health of the DAO.</p></div></div><div class="bloc-ab"><div class="bloc-a"><img src=\'/BanklessDAO/lesson/banklessdao-constitution/membership-levels-fe38ae52.png\'></div><div class="bloc-b"><h2><strong>Whales (L3)</strong></h2><p>Bankless whales hold more than 150k BANK. The membership level grants members access to additional Discord channels.</p></div></div><div class="bloc-ab"><div class="bloc-a"><img src=\'/BanklessDAO/lesson/banklessdao-constitution/membership-levels-467a5425.png\'></div><div class="bloc-b"><h2><strong>Liquidity Providers (L4)</strong></h2><p>Liquidity providers provide Liquidity for the Eth/Bank pair, representing at least 150k Bank and its equivalent in ETH. Grants access to additional Discord channels.</p></div></div>'
+        content: '<div class="bloc-ab"><div class="bloc-a"><img src=\'/BanklessDAO/lesson/banklessdao-constitution/membership-levels-b13f5565.png\'></div><div class="bloc-b"><h2><strong>Contributors (L2)</strong></h2><p>Contributor status is achieved via nomination from one\'s peers. L2s are expected to contribute to the overall health of the DAO and are recognized as valuable members of our community.</p></div></div><div class="bloc-ab"><div class="bloc-a"><img src=\'/BanklessDAO/lesson/banklessdao-constitution/membership-levels-fe38ae52.png\'></div><div class="bloc-b"><h2><strong>Whales (L3)</strong></h2><p>Bankless whales hold more than 150K BANK. This membership level grants access to additional Discord channels.</p></div></div><div class="bloc-ab"><div class="bloc-a"><img src=\'/BanklessDAO/lesson/banklessdao-constitution/membership-levels-467a5425.png\'></div><div class="bloc-b"><h2><strong>Liquidity Providers (L4)</strong></h2><p>These members provide at least 150K BANK and the required ETH to the ETH/BANK liquidity pools. This membership level grants access to additional Discord channels.</p></div></div>'
       },
       {
         type: 'QUIZ',
         notionId: '596c1836a71f4a6d8becb5378cb92c42',
         title: '✅ Knowledge Check',
         quiz: {
-          question: 'Which membership designation can only be achieved through nomination? ',
+          question: 'Which membership designation can only be achieved through nomination?',
           rightAnswerNumber: 2,
           answers: [
             'L1 - Member',
             'L2 - Contributor',
             'L3 - Whale',
-            'L4 - Liquidy Provider'
+            'L4 - Liquidity Provider'
           ],
           id: 'banklessdao-constitution-3'
         }
@@ -164,7 +164,7 @@ const LESSONS: LessonType[] = [
         type: 'LEARN',
         notionId: 'ef6e9463e8a0414b926ce9c9c55af5b2',
         title: '<strong>Projects</strong>',
-        content: '<div class="bloc1"><p>While Guilds serve as talent pools, projects act as objective-based initiatives. They are product or service-oriented value streams designed to make an impact and accomplish some defined end. Anyone can start a project. Projects may seek funding by submitting proposals through the DAOs governance process. Each project has a project champion who is uniquely responsible to report and account for that project\'s status and expenditures.</p></div><div class="bloc2"><img src=\'/BanklessDAO/lesson/banklessdao-constitution/projects-9b285c8a.png\'></div>'
+        content: '<div class="bloc1"><p>While Guilds serve as talent pools, projects act as objective-based initiatives. They are product or service-oriented value streams designed to make an impact and accomplish some defined end. Anyone can start a project. Projects may seek funding by submitting proposals through the DAOs governance process. Each project has a project champion who is uniquely responsible to report and account for that project\'s status and expenditures.</p></div><div class="bloc2"><img src=\'/BanklessDAO/lesson/banklessdao-constitution/projects-62528474.png\'></div>'
       },
       {
         type: 'LEARN',
