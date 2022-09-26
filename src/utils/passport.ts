39a00e8a23977715bb91df104ae3a9ce0b00e7d2
Didier Krux <didier@krux.co>
2022-09-26 17:26:40 +0100

    improve filterValidStamps (avoid duplicates)

--- src/utils/passport.ts
+++ src/utils/passport.ts
@@ -7,14 +7,19 @@ const ALLOWED_PROVIDERS = Object.keys(STAMP_PROVIDERS)
 export const filterValidStamps = (stamps: Stamp[]): Stamp[] => {
   // const currentTimestamp = 1665401965000
   const currentTimestamp = Date.now()
-  return (
-    stamps?.filter(
-      (stamp) =>
-        stamp.credential.issuer === ALLOWED_ISSUER &&
-        ALLOWED_PROVIDERS.includes(stamp.provider) &&
-        Date.parse(stamp.credential.expirationDate) > currentTimestamp
-    ) || []
-  )
+  const filteredStamps = []
+  for (let i = 0; i < stamps.length; i++) {
+    const stamp = stamps[i]
+    if (
+      stamp.credential.issuer === ALLOWED_ISSUER &&
+      ALLOWED_PROVIDERS.includes(stamp.provider) &&
+      !stamps.slice(i + 1).find((s) => s.provider === stamp.provider) &&
+      Date.parse(stamp.credential.expirationDate) > currentTimestamp
+    ) {
+      filteredStamps.push(stamp)
+    }
+  }
+  return filteredStamps
 }
 
 export const getNumberOfValidStamps = (stamps: Stamps): number | null => {
