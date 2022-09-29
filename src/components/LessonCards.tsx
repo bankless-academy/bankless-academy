fcec99a1a8eb37cbc2f9adc8d49414a2570778fb
Didier Krux <didier@krux.co>
2022-09-29 17:59:57 +0100

    improve lesson select UI

--- src/components/LessonCards.tsx
+++ src/components/LessonCards.tsx
@@ -1,11 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import NextLink from 'next/link'
-import { Box, Text, Tag, Image, TagRightIcon } from '@chakra-ui/react'
+import { Box, Text, Tag, Image, TagRightIcon, Button } from '@chakra-ui/react'
 import styled from '@emotion/styled'
 import axios from 'axios'
 import { CircleWavyCheck } from 'phosphor-react'
-import ReactHtmlParser from 'react-html-parser'
-import { useMediaQuery } from '@chakra-ui/react'
 import { useRouter } from 'next/router'
 
 import { LESSONS, IS_WHITELABEL } from 'constants/index'
@@ -26,7 +24,6 @@ const LessonCards: React.FC = () => {
   const { all, slug } = router.query
 
   const [stats, setStats]: any = useState(null)
-  const [isSmallScreen] = useMediaQuery('(max-width: 800px)')
 
   const moduleId = MODULES.find((m) => m.slug === slug)?.moduleId
 
@@ -58,7 +55,10 @@ const LessonCards: React.FC = () => {
         // lesson not started yet: -1
         // const currentSlide = parseInt(localStorage.getItem(lesson.slug) || '-1')
         // const numberOfSlides = lesson.slides.length
-        const isPoapClaimed = localStorage.getItem(`poap-${lesson.slug}`)
+        const isKudosMinted = localStorage.getItem(
+          `isKudosMinted-${lesson.kudosId}`
+        )
+        const isLessonStarted = (localStorage.getItem(lesson.slug) || 0) > 0
         const lessonCompleted =
           (lesson.quest &&
             stats?.lessonCompleted &&
@@ -69,20 +69,17 @@ const LessonCards: React.FC = () => {
             key={`lesson-${index}`}
             p={4}
             pb={8}
-            borderBottom={
-              isSmallScreen && index + 1 < LESSONS.length
-                ? '1px solid #72757b'
-                : ''
-            }
+            border="1px solid #72757b"
+            borderRadius="3xl"
           >
             <Text fontSize="xl" fontWeight="bold">
               {lesson.name}
             </Text>
             <Text fontSize="lg">{lesson.description}</Text>
             <Box display="flex" justifyContent="space-between" my="4">
-              <Tag size="sm" variant={isPoapClaimed ? 'solid' : 'outline'}>
-                {isPoapClaimed ? 'Done' : `${lesson.duration} minutes`}
-                {isPoapClaimed ? (
+              <Tag size="sm" variant={isKudosMinted ? 'solid' : 'outline'}>
+                {isKudosMinted ? 'Done' : `${lesson.duration} minutes`}
+                {isKudosMinted ? (
                   <TagRightIcon as={CircleWavyCheck} weight="bold" />
                 ) : null}
               </Tag>
@@ -103,9 +100,17 @@ const LessonCards: React.FC = () => {
                 <Image src={lesson.lessonImageLink} />
               </LessonBanner>
             </NextLink>
-            <Text fontSize="md" mt="4">
-              {ReactHtmlParser(lesson.learnings)}
-            </Text>
+            <Box display="flex" flexDirection="row-reverse">
+              <NextLink href={`/lessons/${lesson.slug}`}>
+                <Button variant="primary" mt="4">
+                  {isKudosMinted
+                    ? 'Review Lesson'
+                    : isLessonStarted
+                    ? 'Resume Lesson'
+                    : 'Start Lesson'}
+                </Button>
+              </NextLink>
+            </Box>
           </LessonCard>
         )
       })}
