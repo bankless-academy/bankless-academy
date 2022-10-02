7f128fb71ea4fd3d8748dd6ca0ed0d5c62fae0f0
Didier Krux <didier@krux.co>
2022-10-02 16:24:24 +0100

    add logs

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 // middleware.ts
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
@@ -5,6 +6,8 @@ import type { NextRequest } from 'next/server'
 export function middleware(request: NextRequest) {
   const ipAddress = request.headers['x-real-ip'] || 'local'
   // redirect API calls if maintenance in progress
+  console.log('NEXT_PUBLIC_MAINTENANCE', process.env.NEXT_PUBLIC_MAINTENANCE)
+  console.log('ipAddress', ipAddress)
   if (
     process.env.NEXT_PUBLIC_MAINTENANCE &&
     process.env.NEXT_PUBLIC_MAINTENANCE !== ipAddress &&
