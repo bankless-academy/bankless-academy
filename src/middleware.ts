49d330d1366929cc8ab1fb1b21eb009ab58dc9ef
Didier Krux <didier@krux.co>
2022-10-18 19:11:25 +0100

    add bot detection in API

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,18 @@
+/* eslint-disable no-console */
 // middleware.ts
 import { NextResponse } from 'next/server'
+import { userAgent } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest): NextResponse {
   const ipAddress = request.ip || 'local'
+  // Detect and redirect bots
+  const { isBot } = userAgent(request)
+  console.log('url', request.url)
+  console.log('isBot', isBot)
+  if (request.url.includes('/api/passport') && !isBot) {
+    return NextResponse.redirect(new URL('/maintenance', request.url))
+  }
   // disable API calls if maintenance in progress
   if (
     process.env.NEXT_PUBLIC_MAINTENANCE &&
