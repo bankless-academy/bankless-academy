3e58ac101aa6dbddc2dfb03d9f920ec4599194e7
Didier Krux <didier@krux.co>
2022-11-24 12:13:09 +0000

    hide testnet networks on production

--- src/constants/networks.ts
+++ src/constants/networks.ts
@@ -1,4 +1,28 @@
 import { INFURA_ID } from 'constants/index'
+import { IS_MINTKUDOS_SANDBOX } from 'constants/kudos'
+
+const TESTNET_NETWORKS = IS_MINTKUDOS_SANDBOX
+  ? {
+      kovan: {
+        name: 'Kovan Testnet',
+        image: '/images/eth-test.svg',
+        chainId: 42,
+        rpcUrl: `https://kovan.infura.io/v3/${INFURA_ID}`,
+        faucet: 'https://ethdrop.dev/',
+        blockExplorer: 'https://kovan.etherscan.io/',
+      },
+      mumbai: {
+        name: 'Mumbai Testnet',
+        image: '/images/matic-test.svg',
+        networkName: 'Matic(Polygon) Testnet Mumbai',
+        currencySymbol: 'MATIC',
+        chainId: 80001,
+        rpcUrl: `https://polygon-mumbai.infura.io/v3/${INFURA_ID}`,
+        faucet: 'https://faucet.matic.network/',
+        blockExplorer: 'https://mumbai.polygonscan.com/',
+      },
+    }
+  : {}
 
 export const NETWORKS = Object.freeze({
   mainnet: {
@@ -7,14 +31,6 @@ export const NETWORKS = Object.freeze({
     chainId: 1,
     rpcUrl: `https://mainnet.infura.io/v3/${INFURA_ID}`,
     blockExplorer: 'https://etherscan.io/',
-  },
-  kovan: {
-    name: 'Kovan Testnet',
-    image: '/images/eth-test.svg',
-    chainId: 42,
-    rpcUrl: `https://kovan.infura.io/v3/${INFURA_ID}`,
-    faucet: 'https://ethdrop.dev/',
-    blockExplorer: 'https://kovan.etherscan.io/',
   },
   matic: {
     name: 'Polygon',
@@ -26,16 +42,7 @@ export const NETWORKS = Object.freeze({
     faucet: 'https://faucet.matic.network/',
     blockExplorer: 'https://polygonscan.com',
   },
-  mumbai: {
-    name: 'Mumbai Testnet',
-    image: '/images/matic-test.svg',
-    networkName: 'Matic(Polygon) Testnet Mumbai',
-    currencySymbol: 'MATIC',
-    chainId: 80001,
-    rpcUrl: `https://polygon-mumbai.infura.io/v3/${INFURA_ID}`,
-    faucet: 'https://faucet.matic.network/',
-    blockExplorer: 'https://mumbai.polygonscan.com/',
-  },
+  ...TESTNET_NETWORKS,
 })
 
 export const SUPPORTED_NETWORKS_IDS = Object.values(NETWORKS).map(
