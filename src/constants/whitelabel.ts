f32596a6ba5f486479f9ba23acf3fde9335e17a1
Didier Krux <didier@krux.co>
2023-01-12 11:02:07 +0000

    update Notion disclaimer

--- src/constants/whitelabel.ts
+++ src/constants/whitelabel.ts
@@ -11,5 +11,9 @@ export const WHITELABEL: WhitelabelType = {
   homepage_background: '/BanklessDAO/homepage_background-80df8c6d.png',
   logo_small: '/BanklessDAO/logo-7ee3f640.png',
   umami_prod: 'd9ea7467-a2cf-41ac-8ff3-086bb17046bf',
-  twitter_account: 'BanklessDAO'
+  twitter_account: 'BanklessDAO',
+  notion_pages: {
+    faq: '97b88d72335a41a1911c12d4e2f99db6',
+    disclaimer: 'a214bfabc44f4820ab6fa65e2b1f4dfe',
+  }
 }
