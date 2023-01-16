1d96f91106b24cc2562fc221a1f6f902213f428f
Didier Krux <didier@krux.co>
2023-01-16 18:46:15 +0000

    add gold button to theme

--- src/theme/components/index.tsx
+++ src/theme/components/index.tsx
@@ -1,11 +1,14 @@
 import Button from 'theme/components/button'
 import Tag from 'theme/components/tag'
 import Link from 'theme/components/link'
+import { StyleConfig } from '@chakra-ui/theme-tools'
+
+const components: Record<string, StyleConfig> = {
+  ...Button,
+  ...Link,
+  ...Tag,
+}
 
 export default {
-  components: {
-    ...Button,
-    ...Link,
-    ...Tag,
-  },
+  components,
 }
