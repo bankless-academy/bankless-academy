60212707ba5e4f29a18aaa7b85702f41d3d2c145
Didier Krux <didier@krux.co>
2023-02-02 18:56:45 +0000

    add support for plural keywords

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -350,15 +350,19 @@ const Lesson = ({
         )
     }
     if (node.type === 'tag' && node.name === 'code') {
+      // Tooltip with definition
       const keyword = node.children[0]?.data
       const lowerCaseKeyword = node.children[0]?.data?.toLowerCase()
-      // Tooltip with definition
-      return lowerCaseKeyword?.length && lowerCaseKeyword in keywords ? (
-        <Tooltip
-          hasArrow
-          label={keywords[lowerCaseKeyword]?.definition}
-          closeOnClick={false}
-        >
+      const lowerCaseKeywordSingular =
+        lowerCaseKeyword?.length && lowerCaseKeyword.endsWith('s')
+          ? lowerCaseKeyword.slice(0, -1)
+          : undefined
+      if (!lowerCaseKeyword?.length) return <>{keyword}</>
+      const definition =
+        keywords[lowerCaseKeyword]?.definition ||
+        keywords[lowerCaseKeywordSingular]?.definition
+      return definition?.length ? (
+        <Tooltip hasArrow label={definition} closeOnClick={false}>
           <span className="keyword">{keyword}</span>
         </Tooltip>
       ) : (
