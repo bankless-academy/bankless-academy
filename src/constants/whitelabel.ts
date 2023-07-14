4624cf25b9bfa1040a3ea05cc0d09c4802b26fca
Didier Krux <didier@krux.co>
2023-07-14 17:27:43 +0100

    update bDAO config

--- src/constants/whitelabel.ts
+++ src/constants/whitelabel.ts
@@ -1,14 +1,14 @@
 import { WhitelabelType } from 'entities/whitelabel'
 
 export const WHITELABEL: WhitelabelType = {
-  project_name: 'BanklessDAO Academy',
-  domain_prod: 'academy.bankless.community',
-  default_metadata_description: 'Level up your Bankless skills',
+  project_name: 'BanklessDAO Onboarding',
+  domain_prod: 'onboarding.bankless.community',
+  default_metadata_description: 'Level up your skills & onboard to BanklessDAO projects & guilds.',
   default_metadata_image: '/BanklessDAO/image-f7b3de29.jpg',
   favicon: '/BanklessDAO/favicon-8166eefe.png',
-  apple_touch_icon: '/BanklessDAO/apple-touch-icon.png',
   logo: '/BanklessDAO/logo-7ee3f640.png',
   homepage_background: '/BanklessDAO/homepage_background-80df8c6d.png',
+  mintkudos_community_admin: '0xe1887ff140bfa9d3b45d0b2077b7471124acd242',
   logo_small: '/BanklessDAO/logo-7ee3f640.png',
   umami_prod: 'd9ea7467-a2cf-41ac-8ff3-086bb17046bf',
   twitter_account: 'BanklessDAO',
