524bd3e72715d01ef93d8c503aa6cba9a0d5564b
Didier Krux <didier@krux.co>
2023-07-25 18:18:42 +0100

    avoid error with empty nftLink

--- src/components/NFT.tsx
+++ src/components/NFT.tsx
@@ -2,7 +2,7 @@ import { Box, Image as ChakraImage } from '@chakra-ui/react'
 
 const NFT = ({ nftLink }: { nftLink: string }): React.ReactElement => (
   <Box>
-    {nftLink.includes('.mp4') ? (
+    {nftLink?.includes('.mp4') ? (
       <video autoPlay loop playsInline muted>
         <source src={nftLink} type="video/mp4"></source>
       </video>
