93333580c7c8cabf1d5ae16bddebcfbc5ce6bb36
Didier Krux <didier@krux.co>
2023-07-30 14:09:25 +0100

    update image width

--- src/components/Disclaimer.tsx
+++ src/components/Disclaimer.tsx
@@ -67,7 +67,7 @@ const Disclaimer = ({
             <Box mb="4" fontWeight="bold">
               Do you accept full responsibility for your own Bankless journey?
             </Box>
-            <Box mb="4" maxW="80%" m="auto">
+            <Box mb="4" maxW="90%" m="auto">
               <video autoPlay loop playsInline muted>
                 <source
                   src="https://openseauserdata.com/files/fafb924293e3b909b4eb5c803fe11e6e.mp4"
