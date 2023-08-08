7fdf19b7f45c7ef800e3f4f066d8de6ca0172244
Didier Krux <didier@krux.co>
2023-08-08 09:39:51 +0100

    dynamic badge API w/ historic badges

--- src/pages/api/badges.ts
+++ src/pages/api/badges.ts
@@ -1,16 +1,22 @@
 /* eslint-disable no-console */
 import { NextApiRequest, NextApiResponse } from 'next'
-import badges from 'mock/badges.json'
+import badges from 'data/badges.json'
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> {
   // const { address } = req.body
-  const { address } = req.query
-  // console.log(req)
-  if (!address) return res.status(400).json({ error: 'Wrong params' })
-  console.log('address', address)
+  let { address } = req.query
 
-  return res.status(200).json(badges)
+  if (typeof address === 'string') {
+    address = address.toLowerCase()
+    // console.log(req)
+    if (!address) return res.status(400).json({ error: 'Wrong params' })
+    // console.log('address', address)
+
+    const badgeTokenIds = address in badges ? badges[address] : []
+
+    return res.status(200).json({ badgeTokenIds })
+  }
 }
