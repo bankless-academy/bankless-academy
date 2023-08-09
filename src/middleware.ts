d5b4be8c804aed2c6e84c4b7ab622bd76f1a43f9
Didier Krux <didier@krux.co>
2023-08-09 20:29:13 +0100

    rollback to next 12

---
+++ src/middleware.ts
@@ -0,0 +1,30 @@
+/* eslint-disable no-console */
+// middleware.ts
+// TODO: migrate to https://github.com/neet/next-composition
+import { NextResponse } from 'next/server'
+import { userAgent } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export function middleware(request: NextRequest): NextResponse {
+  const ipAddress = request.ip || 'local'
+  // Detect and redirect bots
+  const ua = userAgent(request)
+  console.log('url', request.url)
+  console.log('userAgent', ua)
+  if (ua.ua.includes('python') || ua.ua.includes('curl')) {
+    if (request.url.includes('/api/passport')) return NextResponse.next()
+    else return NextResponse.redirect(new URL('/maintenance', request.url))
+  }
+  // disable API calls if maintenance in progress
+  if (
+    process.env.NEXT_PUBLIC_MAINTENANCE &&
+    process.env.NEXT_PUBLIC_MAINTENANCE !== ipAddress &&
+    ipAddress !== 'local'
+  ) {
+    return NextResponse.redirect(new URL('/maintenance', request.url))
+  } else return NextResponse.next()
+}
+
+export const config = {
+  matcher: ['/api/:path*'],
+}
