1637aeabea48554cc54b4e76242ef983057df2d1
Didier Krux <didier@krux.co>
2023-08-17 18:57:59 +0100

    add badge-contrat-uri metadata

--- src/pages/api/metadata/[...slug].ts
+++ src/pages/api/metadata/[...slug].ts
@@ -13,12 +13,25 @@ export default async function handler(
   const {
     slug: [slug, tokenId],
   } = req.query
+
+  if (slug === 'badge-contrat-uri') {
+    const metadata = {
+      "name": "Bankless Academy Badges",
+      "symbol": "BADGE",
+      "description": "Bankless Academy lesson badges https://app.banklessacademy.com/",
+      "image": "https://app.banklessacademy.com/logo.jpg",
+    }
+    return res.status(200).json(metadata)
+  }
+
   if (!slug || !tokenId) return res.status(400).json({ error: 'Wrong params' })
+
   if (!['badge', 'badgev2'].includes(slug))
     return res.status(400).json({ error: 'Unknown contract' })
   const lesson = LESSONS.find(
     (lesson) => lesson.badgeId === parseInt(tokenId, 10)
   )
+
   if (!lesson) return res.status(400).json({ error: 'Unknown tokenId' })
 
   const metadata = {
