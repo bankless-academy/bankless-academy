42948cee278c91cbda6c81e0bfdb260759b5e452
Didier Krux <didier@krux.co>
2023-08-21 17:59:20 +0100

    separate ERC1155SupplyUpgradeable.sol file

--- contracts/BanklessAcademyBadges.sol
+++ contracts/BanklessAcademyBadges.sol
@@ -22,7 +22,7 @@ import '@openzeppelin/contracts-upgradeable/token/ERC1155/ERC1155Upgradeable.sol
 import '@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol';
 import '@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol';
 import '@openzeppelin/contracts-upgradeable/token/ERC1155/extensions/ERC1155BurnableUpgradeable.sol';
-import '@openzeppelin/contracts-upgradeable/token/ERC1155/extensions/ERC1155SupplyUpgradeable.sol';
+import './token/ERC1155/extensions/ERC1155SupplyUpgradeable.sol';
 import '@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol';
 import '@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol';
 
