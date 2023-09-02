52c00d388d5d2045eba64441c719861f23980976
Didier Krux <didier@krux.co>
2023-09-02 18:18:45 +0100

    generate translation files with i18next-parser

---
+++ generate-translation-files.sh
@@ -0,0 +1,10 @@
+# generate all translation files via command line
+
+# common
+i18next \"$(grep -l "useTranslation()" -R src/* | tr '\n', ' ' | sed 's/.$//' | sed 's/ /" "/g')\" -o translation/website/en/common.json
+
+# homepage
+i18next src/pages/index.tsx -o translation/website/en/homepage.json
+
+# quests
+i18next src/components/Quest/*.tsx -o translation/website/en/quests.json
