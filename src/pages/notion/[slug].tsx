e3330ebe1b9122e2199dfa72e253fd6bbdf3a292
Didier Krux <didier@krux.co>
2023-09-03 12:11:38 +0100

    typo

--- src/pages/notion/[slug].tsx
+++ src/pages/notion/[slug].tsx
@@ -63,7 +63,7 @@ const NotionPage = ({
     return (
       <div>
         <h3>No data found.</h3>
-        <div> Make sure the pageId is valid.</div>
+        <div>Make sure the pageId is valid.</div>
         <div>Only public pages are supported in this example.</div>
       </div>
     )
