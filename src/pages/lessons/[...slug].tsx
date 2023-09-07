8aee200a3eb3f5b91b578e42cbfb64edd155b572
Didier Krux <didier@krux.co>
2023-09-07 15:39:21 +0100

    fix english lesson loading

--- src/pages/lessons/[...slug].tsx
+++ src/pages/lessons/[...slug].tsx
@@ -105,42 +105,46 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   const slug = params.slug.length === 1 ? params.slug[0] : params.slug[1]
   const language: any = params.slug.length === 1 ? 'en' : params.slug[0]
   let currentLesson = LESSONS.find((lesson: LessonType) => lesson.slug === slug)
-  console.log(currentLesson)
-  for (const language of currentLesson.languages) {
-    if (
-      !fs.existsSync(`translation/lesson/${language}/${currentLesson.slug}.md`)
-    ) {
-      currentLesson.languages = currentLesson.languages.filter(
-        (l) => l !== language
-      )
-    }
-  }
-  try {
-    if (
-      fs.existsSync(`translation/lesson/${language}/${currentLesson.slug}.md`)
-    ) {
-      const md = await fs.readFileSync(
-        `translation/lesson/${language}/${currentLesson.slug}.md`,
-        'utf8'
-      )
-      if (md && md.includes('TITLE:') && currentLesson) {
-        console.log('processMD start')
-        currentLesson = await processMD(md, language, currentLesson)
-        currentLesson.lang = language
+  // console.log(currentLesson)
+  if (language !== 'en') {
+    for (const language of currentLesson.languages) {
+      if (
+        !fs.existsSync(
+          `translation/lesson/${language}/${currentLesson.slug}.md`
+        )
+      ) {
+        currentLesson.languages = currentLesson.languages.filter(
+          (l) => l !== language
+        )
       }
     }
-  } catch (error) {
-    const pageMeta: MetaData = {
-      title: currentLesson.name,
-      description: currentLesson.description,
-      image: currentLesson.socialImageLink || DEFAULT_METADATA.image,
-      isLesson: !currentLesson.isArticle,
-      lesson: currentLesson,
-    }
-    console.log('error loading language', language)
-    console.log('error', error)
-    return {
-      props: { pageMeta },
+    try {
+      if (
+        fs.existsSync(`translation/lesson/${language}/${currentLesson.slug}.md`)
+      ) {
+        const md = await fs.readFileSync(
+          `translation/lesson/${language}/${currentLesson.slug}.md`,
+          'utf8'
+        )
+        if (md && md.includes('TITLE:') && currentLesson) {
+          console.log('processMD start')
+          currentLesson = await processMD(md, language, currentLesson)
+          currentLesson.lang = language
+        }
+      }
+    } catch (error) {
+      const pageMeta: MetaData = {
+        title: currentLesson.name,
+        description: currentLesson.description,
+        image: currentLesson.socialImageLink || DEFAULT_METADATA.image,
+        isLesson: !currentLesson.isArticle,
+        lesson: currentLesson,
+      }
+      console.log('error loading language', language)
+      console.log('error', error)
+      return {
+        props: { pageMeta },
+      }
     }
   }
 
