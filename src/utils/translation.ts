bd1f60e37f0c21917ac982a3927eb1ccf5036877
Didier Krux <didier@krux.co>
2023-10-03 16:26:28 +0100

    add it translation

--- src/utils/translation.ts
+++ src/utils/translation.ts
@@ -8,19 +8,12 @@ import homepage from '../../translation/website/en/homepage.json'
 import quests from '../../translation/website/en/quests.json'
 import keywords from '../../translation/keywords/en/keywords.json'
 
-// FR
-import commonFR from '../../translation/website/fr/common.json'
-import homepageFR from '../../translation/website/fr/homepage.json'
-import questsFR from '../../translation/website/fr/quests.json'
-import keywordsFR from '../../translation/keywords/fr/keywords.json'
-import lessonFR from '../../translation/website/fr/lesson.json'
-
-// ES
-import commonES from '../../translation/website/es/common.json'
-// import homepageES from '../../translation/website/es/homepage.json'
-import questsES from '../../translation/website/es/quests.json'
-import keywordsES from '../../translation/keywords/es/keywords.json'
-import lessonES from '../../translation/website/es/lesson.json'
+// CN
+import commonCN from '../../translation/website/cn/common.json'
+// import homepageCN from '../../translation/website/cn/homepage.json'
+import questsCN from '../../translation/website/cn/quests.json'
+import keywordsCN from '../../translation/keywords/cn/keywords.json'
+import lessonCN from '../../translation/website/cn/lesson.json'
 
 // DE
 import commonDE from '../../translation/website/de/common.json'
@@ -29,12 +22,26 @@ import questsDE from '../../translation/website/de/quests.json'
 import keywordsDE from '../../translation/keywords/de/keywords.json'
 import lessonDE from '../../translation/website/de/lesson.json'
 
-// CN
-import commonCN from '../../translation/website/cn/common.json'
-// import homepageCN from '../../translation/website/cn/homepage.json'
-import questsCN from '../../translation/website/cn/quests.json'
-import keywordsCN from '../../translation/keywords/cn/keywords.json'
-import lessonCN from '../../translation/website/cn/lesson.json'
+// ES
+import commonES from '../../translation/website/es/common.json'
+// import homepageES from '../../translation/website/es/homepage.json'
+import questsES from '../../translation/website/es/quests.json'
+import keywordsES from '../../translation/keywords/es/keywords.json'
+import lessonES from '../../translation/website/es/lesson.json'
+
+// FR
+import commonFR from '../../translation/website/fr/common.json'
+import homepageFR from '../../translation/website/fr/homepage.json'
+import questsFR from '../../translation/website/fr/quests.json'
+import keywordsFR from '../../translation/keywords/fr/keywords.json'
+import lessonFR from '../../translation/website/fr/lesson.json'
+
+// IT
+// import commonIT from '../../translation/website/it/common.json'
+// import homepageIT from '../../translation/website/it/homepage.json'
+// import questsIT from '../../translation/website/it/quests.json'
+// import keywordsIT from '../../translation/keywords/it/keywords.json'
+import lessonIT from '../../translation/website/it/lesson.json'
 
 // JP
 import commonJP from '../../translation/website/jp/common.json'
@@ -58,19 +65,12 @@ i18next
         quests,
         keywords,
       },
-      fr: {
-        common: commonFR,
-        homepage: homepageFR,
-        quests: questsFR,
-        keywords: keywordsFR,
-        lesson: lessonFR,
-      },
-      es: {
-        common: commonES,
-        // homepage: homepageES,
-        quests: questsES,
-        keywords: keywordsES,
-        lesson: lessonES,
+      cn: {
+        common: commonCN,
+        // homepage: homepageCN,
+        quests: questsCN,
+        keywords: keywordsCN,
+        lesson: lessonCN,
       },
       de: {
         common: commonDE,
@@ -79,12 +79,26 @@ i18next
         keywords: keywordsDE,
         lesson: lessonDE,
       },
-      cn: {
-        common: commonCN,
-        // homepage: homepageCN,
-        quests: questsCN,
-        keywords: keywordsCN,
-        lesson: lessonCN,
+      es: {
+        common: commonES,
+        // homepage: homepageES,
+        quests: questsES,
+        keywords: keywordsES,
+        lesson: lessonES,
+      },
+      fr: {
+        common: commonFR,
+        homepage: homepageFR,
+        quests: questsFR,
+        keywords: keywordsFR,
+        lesson: lessonFR,
+      },
+      it: {
+        // common: commonIT,
+        // homepage: homepageIT,
+        // quests: questsIT,
+        // keywords: keywordsIT,
+        lesson: lessonIT,
       },
       jp: {
         common: commonJP,
