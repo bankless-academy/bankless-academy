af4520549b034d848b68d2de288bcaabab63468e
Didier Krux <didier@krux.co>
2023-10-31 19:46:03 +0000

    fix webpack fs issue

---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+      };
+    }
+
+    return config;
+  },
+}
