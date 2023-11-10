dc24ae167d8b59e5a440dda3d7efb6f92296feb5
Didier Krux <didier@krux.co>
2023-11-10 18:41:58 +0000

    improve profile page

--- src/pages/profile/[address].tsx
+++ src/pages/profile/[address].tsx
@@ -1,8 +1,11 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Heading, Image } from '@chakra-ui/react'
 import Badges from 'components/Badges'
 import { MetaData } from 'components/Head'
-import { DOMAIN_URL } from 'constants/index'
+import { ALCHEMY_KEY_BACKEND, DOMAIN_URL } from 'constants/index'
 import { TABLES, db } from 'utils/db'
+import { createPublicClient, http } from 'viem'
+import { mainnet } from 'viem/chains'
+import { normalize } from 'viem/ens'
 
 /* eslint-disable no-console */
 export async function getServerSideProps({ query }) {
@@ -10,21 +13,33 @@ export async function getServerSideProps({ query }) {
 
   let error = ''
   if (!address) error = 'missing address'
+  const transport = http(
+    `https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_KEY_BACKEND}`
+  )
+  const client = createPublicClient({
+    chain: mainnet,
+    transport,
+  })
+  const fullAddress = address.endsWith('.eth')
+    ? await client.getEnsAddress({ name: normalize(address) })
+    : address
+  console.log(fullAddress)
   const [userExist] = await db(TABLES.users)
-    .select('id')
-    .whereILike('address', address)
+    .select('id', 'address')
+    .whereILike('address', fullAddress)
   console.log('user', userExist)
   if (!userExist) error = 'user not found'
   if (error) return { props: { error } }
+  const confirmedAddress = userExist.address
 
   const DOMAIN =
     process.env.NODE_ENV === 'development'
       ? 'http://localhost:3000'
       : DOMAIN_URL
 
-  console.log(address)
+  console.log(confirmedAddress)
   console.log(badge)
-  const res = await fetch(`${DOMAIN}/api/user/${address}`)
+  const res = await fetch(`${DOMAIN}/api/user/${confirmedAddress}`)
   if (!res.ok) {
     throw new Error('Failed to fetch data')
   }
@@ -34,8 +49,9 @@ export async function getServerSideProps({ query }) {
   if (badge && !badgeTokenIds.includes(parseInt(badge)))
     return { props: { error: 'badge not found' } }
   const data = {
-    address,
+    address: confirmedAddress,
     ensName: user.ensName,
+    avatar: user.avatar,
     badges: badgeTokenIds,
     badgeToHighlight: parseInt(badge),
   }
@@ -43,9 +59,9 @@ export async function getServerSideProps({ query }) {
   const badgeId = badge ? `&badgeId=${badge}` : ''
 
   const pageMeta: MetaData = {
-    title: `profile: ${address}`,
+    title: `profile: ${user.ensName || confirmedAddress}`,
     // description: currentLesson.description,
-    image: `${DOMAIN}/api/og/profile?address=${address}${badgeId}`,
+    image: `${DOMAIN}/api/og/profile?address=${confirmedAddress}${badgeId}`,
   }
 
   return { props: { ...data, pageMeta } }
@@ -54,6 +70,7 @@ export async function getServerSideProps({ query }) {
 export default function Page({
   address,
   ensName,
+  avatar,
   badges,
   badgeToHighlight,
   error,
@@ -64,7 +81,12 @@ export default function Page({
   return (
     <>
       <Box>
-        <h1>{ensName || address}</h1>
+        <Heading as="h2" size="xl" m="8" textAlign="center">
+          {ensName || address}
+        </Heading>
+        <Box display="flex" justifyContent="center" w="100%" mb="8">
+          <Image src={avatar} width="100px" height="100px" borderRadius="50%" />
+        </Box>
         <Badges
           badges={badges}
           badgeToHighlight={badgeToHighlight}
