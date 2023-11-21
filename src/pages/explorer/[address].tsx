844f505a47ddf0eb4bdd23327adaaf57d7a19500
Didier Krux <didier@krux.co>
2023-11-21 13:19:20 +0000

    update profile

--- src/pages/explorer/[address].tsx
+++ src/pages/explorer/[address].tsx
@@ -1,4 +1,4 @@
-import { Box, Container, Image, Text } from '@chakra-ui/react'
+import { Box, Button, Container, Image, Text } from '@chakra-ui/react'
 import { mainnet } from 'viem/chains'
 import { normalize } from 'viem/ens'
 import { createPublicClient, http } from 'viem'
@@ -13,6 +13,9 @@ import { shortenAddress } from 'utils'
 import { TABLES, db } from 'utils/db'
 import { t } from 'i18next'
 import ProgressTitle from 'components/ProgressTitle'
+import ExternalLink from 'components/ExternalLink'
+import router from 'next/router'
+import { useAccount } from 'wagmi'
 
 /* eslint-disable no-console */
 export async function getServerSideProps({ query }) {
@@ -53,7 +56,7 @@ export async function getServerSideProps({ query }) {
   const badgeToHighlight = parseInt(badge)
   const data = {
     user: user,
-    address: confirmedAddress,
+    userAddress: confirmedAddress,
     badgeToHighlight,
   }
   console.log(data)
@@ -62,8 +65,8 @@ export async function getServerSideProps({ query }) {
     title: `Explorer ${user.ensName || shortenAddress(confirmedAddress)}`,
     // description: currentLesson.description,
     image: `${DOMAIN_URL}/api/og/social?address=${confirmedAddress}${
-      badge && `&badge=${badge}&f=1`
-    }`,
+      badge ? `&badge=${badge}` : ''
+    }&f=1`,
   }
 
   return { props: { ...data, pageMeta } }
@@ -71,30 +74,58 @@ export async function getServerSideProps({ query }) {
 
 export default function Page({
   user,
-  address,
+  userAddress,
   badgeToHighlight,
   error,
 }: {
   user: UserType
-  address: string
+  userAddress: string
   badgeToHighlight?: number
   error?: any
 }) {
   const [isSmallScreen] = useSmallScreen()
+  const { referral } = router.query
+  const { address } = useAccount()
 
   const collectibles = []
-  for (let i = 0; i < user.stats.collectibles; i++) {
+  for (let i = 0; i < user?.stats.collectibles; i++) {
     collectibles.push(
       'https://raw.seadn.io/files/df805d7e6cee4f4697207f7a9929a77f.png'
     )
   }
-  for (let i = 0; i < user.stats.handbooks; i++) {
+  for (let i = 0; i < user?.stats.handbooks; i++) {
     collectibles.push('/images/handbook.svg')
   }
   if (error) return error
+
+  const share = `Checkout my @BanklessAcademy Explorer profile ${
+    typeof window !== 'undefined' && window.location.href
+  }?referral=true`
+  const twitterLink = `https://twitter.com/intent/tweet?url=${encodeURIComponent(
+    share
+  )}`
+
+  const wallets = localStorage.getItem('wallets')
+    ? JSON.parse(localStorage.getItem('wallets'))
+    : []
+
+  const isMyProfile = wallets.includes(userAddress?.toLowerCase())
+
+  if (
+    referral === 'true' &&
+    !isMyProfile &&
+    !localStorage.getItem('referral')?.length
+  ) {
+    console.log('referral', localStorage.getItem('referral'))
+    localStorage.setItem('referral', userAddress?.toLowerCase())
+  }
+  if (address && localStorage.getItem('referral') === address?.toLowerCase()) {
+    localStorage.setItem('referral', '')
+  }
+
   return (
     <Container maxW="container.lg">
-      <Card mt="180px" borderRadius="2xl !important" height="280px">
+      <Card mt="180px" borderRadius="2xl !important">
         <Box
           margin="auto"
           mt="-130px"
@@ -119,11 +150,36 @@ export default function Page({
           textAlign="center"
           textTransform="uppercase"
           mt="40px"
+          mb="6"
         >
-          {user.ensName || shortenAddress(address)}
+          {user.ensName || shortenAddress(userAddress)}
         </Text>
+        {isMyProfile && (
+          <Box pb="8" textAlign="center">
+            <ExternalLink href={twitterLink} mb="8">
+              <Button
+                variant="primary"
+                leftIcon={<Image width="24px" src="/images/Twitter.svg" />}
+              >
+                {t('Share on Twitter')}
+              </Button>
+            </ExternalLink>
+          </Box>
+        )}
       </Card>
-      <Card mt="8" borderRadius="2xl !important">
+      <Card my="8" borderRadius="2xl !important">
+        <Box m="auto" position="relative" w="300px">
+          <Image w="300px" src="/images/bankless-score.png" />
+          <Box
+            position="absolute"
+            top="69px"
+            left="227px"
+            fontSize="3xl"
+            fontWeight="bold"
+          >
+            {user.stats.score}
+          </Box>
+        </Box>
         <Box display={isSmallScreen ? 'block' : 'flex'} m="8">
           <Box
             w={isSmallScreen ? '100%' : '50%'}
@@ -138,7 +194,7 @@ export default function Page({
             <Badges
               badges={user.badgeTokenIds}
               badgeToHighlight={badgeToHighlight}
-              profile={true}
+              type="badges"
             />
           </Box>
           <Box w={isSmallScreen ? '100%' : '50%'} maxW="624px">
@@ -147,7 +203,7 @@ export default function Page({
               score={user.stats?.collectibles + user.stats?.handbooks || 0}
               max={8}
             />
-            <Badges badges={collectibles} profile={true} />
+            <Badges badges={collectibles} type="collectibles" />
           </Box>
         </Box>
         <Box display={isSmallScreen ? 'block' : 'flex'} m="8">
@@ -165,7 +221,7 @@ export default function Page({
               }
               max={8}
             />
-            <Badges badges={Object.keys(user.donations)} profile={true} />
+            <Badges badges={Object.keys(user.donations)} type="donations" />
           </Box>
         </Box>
       </Card>
