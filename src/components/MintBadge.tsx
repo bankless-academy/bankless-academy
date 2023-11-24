1b3ebbf736c02cf3e802ae0de3dc9703d7d0e509
Didier Krux <didier@krux.co>
2023-11-24 19:02:04 +0000

    move GitcoinModal to dedicated file

--- src/components/MintBadge.tsx
+++ src/components/MintBadge.tsx
@@ -1,19 +1,6 @@
 /* eslint-disable no-console */
 import { useState, useEffect } from 'react'
-import {
-  Button,
-  useToast,
-  Image,
-  Box,
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  Text,
-  ModalCloseButton,
-  ModalBody,
-  ModalFooter,
-  useDisclosure,
-} from '@chakra-ui/react'
+import { Button, useToast, Box, useDisclosure } from '@chakra-ui/react'
 import axios from 'axios'
 import { useLocalStorage } from 'usehooks-ts'
 import { useAccount } from 'wagmi'
@@ -21,7 +8,6 @@ import { signMessage, waitForTransaction } from '@wagmi/core'
 import { Gear, SealCheck } from '@phosphor-icons/react'
 import { useTranslation } from 'react-i18next'
 
-import Passport from 'components/Passport'
 import ExternalLink from 'components/ExternalLink'
 import { WALLET_SIGNATURE_MESSAGE } from 'constants/index'
 import {
@@ -31,6 +17,7 @@ import {
 } from 'constants/badges'
 import { EMPTY_PASSPORT } from 'constants/passport'
 import { api } from 'utils'
+import GitcoinModal from 'components/GitcoinModal'
 
 const MintBadge = ({ badgeId }: { badgeId: number }): React.ReactElement => {
   const { t } = useTranslation()
@@ -246,128 +233,6 @@ const MintBadge = ({ badgeId }: { badgeId: number }): React.ReactElement => {
     }
   }
 
-  // const ConnectFirst = (
-  //   <>
-  //     <Heading as="h2" size="xl" textAlign="center" pt="8">
-  //       To claim rewards, you must connect your wallet.
-  //     </Heading>
-  //   </>
-  // )
-
-  const GitcoinModal = (
-    <Modal
-      onClose={() => {
-        if (passportLS?.verified) {
-          toast.closeAll()
-          toast({
-            description: (
-              <>
-                <Box>
-                  <Box display="flex">
-                    <Box mr="4">
-                      <Image
-                        src="/images/gitcoin-passport.svg"
-                        alt="Gitcoin Passport"
-                      />
-                    </Box>
-                    <Box flexDirection="column" alignSelf="center">
-                      <Box fontWeight="bold">
-                        {t('Gitcoin Passport successfully set up.')}
-                      </Box>
-                      <Box mt="4">{t('Try to mint your badge again.')}</Box>
-                    </Box>
-                  </Box>
-                </Box>
-              </>
-            ),
-            status: 'success',
-            duration: 10000,
-            isClosable: true,
-          })
-        } else {
-          toast.closeAll()
-          toast({
-            description: (
-              <>
-                <Box>
-                  <Box display="flex">
-                    <Box mr="4">
-                      <Image
-                        src="/images/gitcoin-passport.svg"
-                        alt="Gitcoin Passport"
-                      />
-                    </Box>
-                    <Box flexDirection="column" alignSelf="center">
-                      <Box fontWeight="bold">
-                        {t('Gitcoin Passport not set up.')}
-                      </Box>
-                      <Box mt="4">
-                        <ExternalLink
-                          underline="true"
-                          href="/faq#ea6ae6bd9ca645498c15cc611bc181c0"
-                        >
-                          {t('Follow these steps and try again')}
-                        </ExternalLink>
-                      </Box>
-                    </Box>
-                  </Box>
-                </Box>
-              </>
-            ),
-            status: 'warning',
-            duration: 20000,
-            isClosable: true,
-          })
-        }
-        onClose()
-      }}
-      size={'xl'}
-      isOpen={isOpen}
-      isCentered
-    >
-      <ModalOverlay />
-      <ModalContent
-        bg="linear-gradient(180deg, #a379bdcc 0%, #5a5198cc 100%)"
-        border="2px solid #B68BCC"
-        borderRadius="3xl"
-        backdropFilter="blur(10px)"
-      >
-        <ModalCloseButton />
-        <ModalBody>
-          <Text fontSize="xl">
-            {!passportLS?.verified && (
-              <Box mt="4" mr="4">
-                {t(
-                  'You haven’t set up Gitcoin Passport, or your stamps are out of date.'
-                )}
-              </Box>
-            )}
-            <Box my="4">
-              {t(
-                'Explorers must have a valid Gitcoin Passport in order to collect Bankless Academy rewards.'
-              )}{' '}
-              <ExternalLink
-                underline="true"
-                href="/faq#640071a81daf4aa4b7df00b1eec1c58d"
-              >
-                {t('Learn more')}
-              </ExternalLink>
-            </Box>
-          </Text>
-          <Passport displayStamps />
-        </ModalBody>
-        <ModalFooter>
-          <ExternalLink
-            underline="true"
-            href="/faq#640071a81daf4aa4b7df00b1eec1c58d"
-          >
-            {t('Help')}
-          </ExternalLink>
-        </ModalFooter>
-      </ModalContent>
-    </Modal>
-  )
-
   return (
     <>
       <Button
@@ -386,7 +251,7 @@ const MintBadge = ({ badgeId }: { badgeId: number }): React.ReactElement => {
       >
         {t('Mint Badge')}
       </Button>
-      {GitcoinModal}
+      <GitcoinModal isOpen={isOpen} onClose={onClose} />
     </>
   )
 }
