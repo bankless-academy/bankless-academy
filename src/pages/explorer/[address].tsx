6a728f320b5f90f7a6eca2b630cb4f8085041770
Didier Krux <didier@krux.co>
2023-11-30 17:13:54 +0000

    optimize profile loading

--- src/pages/explorer/[address].tsx
+++ src/pages/explorer/[address].tsx
@@ -1,103 +1,100 @@
+/* eslint-disable no-console */
+import { useEffect, useState } from 'react'
 import {
   Box,
   Button,
   Container,
+  Heading,
   Image,
   Text,
   useClipboard,
   useMediaQuery,
 } from '@chakra-ui/react'
-import { mainnet } from 'viem/chains'
-import { normalize } from 'viem/ens'
-import { createPublicClient, http } from 'viem'
 import { UserPlus } from '@phosphor-icons/react'
+import router from 'next/router'
+import { useAccount } from 'wagmi'
+import { t } from 'i18next'
+import { fetchEnsAddress } from '@wagmi/core'
 
 import Badges from 'components/Badges'
 import Card from 'components/Card'
 import { MetaData } from 'components/Head'
-import { ALCHEMY_KEY_BACKEND, DOMAIN_URL } from 'constants/index'
+import { DOMAIN_URL } from 'constants/index'
 import { UserType } from 'entities/user'
 import { shortenAddress } from 'utils'
-import { TABLES, db } from 'utils/db'
-import { t } from 'i18next'
 import ProgressTitle from 'components/ProgressTitle'
 import ExternalLink from 'components/ExternalLink'
-import router from 'next/router'
-import { useAccount } from 'wagmi'
 
-/* eslint-disable no-console */
 export async function getServerSideProps({ query }) {
   const { address, badge } = query
 
-  let error = ''
-  if (!address) error = 'missing address'
-  if (error) return { props: { error } }
-  const transport = http(
-    `https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_KEY_BACKEND}`
-  )
-  const client = createPublicClient({
-    chain: mainnet,
-    transport,
-  })
-  const fullAddress = address.endsWith('.eth')
-    ? await client.getEnsAddress({ name: normalize(address) })
-    : address
-  // console.log(fullAddress)
-  const [userExist] = await db(TABLES.users)
-    .select('id', 'address')
-    .whereILike('address', fullAddress)
-  console.log('user', userExist)
-  if (!userExist) error = 'user not found'
-  if (error) return { props: { error } }
-  const confirmedAddress = userExist.address
-
-  // console.log(confirmedAddress)
-  // console.log(badge)
-  const res = await fetch(`${DOMAIN_URL}/api/user/${confirmedAddress}`)
-  if (!res.ok) {
-    throw new Error('Failed to fetch data')
-  }
-  const user: UserType = await res.json()
-  // console.log(user)
-  const badgeTokenIds = user.badgeTokenIds
-  if (badge && !badgeTokenIds.includes(parseInt(badge)))
-    return { props: { error: 'badge not found' } }
+  let preloadError = ''
+  if (!address) preloadError = 'missing address'
+  if (preloadError) return { props: { preloadError } }
   const badgeToHighlight = parseInt(badge)
   const data = {
-    user: user,
-    userAddress: confirmedAddress,
+    profileAddress: address,
     badgeToHighlight,
   }
   // console.log(data)
 
+  const random = Math.floor(Math.random() * 100000)
+
   const pageMeta: MetaData = {
-    title: `Explorer ${user.ensName || shortenAddress(confirmedAddress)}`,
-    // description: currentLesson.description,
-    image: `${DOMAIN_URL}/api/og/social?address=${confirmedAddress}${
+    title: `Explorer ${address}`,
+    description: `${badge ? 'Explorer badge' : 'Explorer profile'}`,
+    image: `${DOMAIN_URL}/api/og/social?address=${address}${
       badge ? `&badge=${badge}` : ''
-    }&score=${user?.stats?.score || 0}&f=1`,
+    }&r=${random}`,
   }
 
   return { props: { ...data, pageMeta } }
 }
 
 export default function Page({
-  user,
-  userAddress,
+  profileAddress,
   badgeToHighlight,
-  error,
+  preloadError,
 }: {
-  user: UserType
-  userAddress: string
+  profileAddress: string
   badgeToHighlight?: number
-  error?: any
+  preloadError?: string
 }) {
   const profileUrl =
     typeof window !== 'undefined' ? `${window.location.href}?referral=true` : ''
   const [isSmallScreen] = useMediaQuery(['(max-width: 981px)'])
   const { referral } = router.query
+  const [user, setUser] = useState<UserType | null>(null)
+  const [error, setError] = useState(preloadError)
+  const [fullProfileAddress, setFullProfileAddress] = useState('')
   const { address } = useAccount()
   const { onCopy, hasCopied } = useClipboard(profileUrl)
+
+  useEffect(() => {
+    const loadUser = async () => {
+      try {
+        const fullAddress = profileAddress?.endsWith('.eth')
+          ? (await fetchEnsAddress({ name: profileAddress }))?.toLowerCase()
+          : profileAddress?.toLowerCase()
+        console.log(fullAddress)
+        if (!fullAddress) setError('No address found')
+        else {
+          setFullProfileAddress(fullAddress)
+          const res = await fetch(`/api/user/${fullAddress}`)
+          if (!res.ok) {
+            setError('Failed to fetch user data.')
+          }
+          const user: UserType = await res.json()
+          if (user) setUser(user)
+          console.log(user)
+        }
+      } catch (error) {
+        console.log(error)
+        setError('Failed to fetch user data from API.')
+      }
+    }
+    loadUser()
+  }, [])
 
   const collectibles = []
   for (let i = 0; i < user?.stats.datadisks?.length; i++) {
@@ -106,7 +103,6 @@ export default function Page({
   for (let i = 0; i < user?.stats.handbooks?.length; i++) {
     collectibles.push(user?.stats.handbooks[i])
   }
-  if (error) return error
 
   const share = `Checkout my @BanklessAcademy Explorer profile ${
     typeof window !== 'undefined' && window.location.href
@@ -119,178 +115,197 @@ export default function Page({
     ? JSON.parse(localStorage.getItem('wallets'))
     : []
 
-  const isMyProfile = wallets.includes(userAddress?.toLowerCase())
+  const isMyProfile =
+    fullProfileAddress !== '' && wallets.includes(fullProfileAddress)
 
   if (
     referral === 'true' &&
     !isMyProfile &&
-    !localStorage.getItem('referral')?.length
+    !localStorage.getItem('referral')?.length &&
+    fullProfileAddress
   ) {
     console.log('referral', localStorage.getItem('referral'))
-    localStorage.setItem('referral', userAddress?.toLowerCase())
+    localStorage.setItem('referral', fullProfileAddress?.toLowerCase())
   }
   if (address && localStorage.getItem('referral') === address?.toLowerCase()) {
     localStorage.setItem('referral', '')
   }
 
-  return (
-    <Container maxW="container.lg">
-      <Card mt="180px" borderRadius="2xl !important">
-        <Box
-          margin="auto"
-          mt="-130px"
-          pt="12px"
-          w="284px"
-          h="284px"
-          borderRadius="50%"
-          backgroundImage="linear-gradient(180deg, #A379BD 0%, #5B5198 100%)"
-        >
-          <Image
-            w="260px"
-            h="260px"
-            margin="auto"
-            borderRadius="50%"
-            backgroundColor="black"
-            src={user.avatar}
-          />
-        </Box>
-        <Text
-          as="h2"
-          fontSize="4xl"
-          fontWeight="bold"
-          textAlign="center"
-          textTransform="uppercase"
-          mt="40px"
-          mb="8"
-        >
-          {user.ensName || shortenAddress(userAddress)}
-        </Text>
-        {isMyProfile && (
-          <Box justifyContent="center" w="256px" m="auto" mb="8">
-            <Box pb="2">
-              <ExternalLink href={twitterLink} mr="2">
-                <Button
-                  variant="primary"
-                  w="100%"
-                  borderBottomRadius="0"
-                  leftIcon={<Image width="24px" src="/images/TwitterX.svg" />}
-                >
-                  {t('Share on Twitter')}
-                </Button>
-              </ExternalLink>
-            </Box>
-            <Button
-              variant="primary"
-              w="100%"
-              borderTopRadius="0"
-              leftIcon={<UserPlus size="30px" />}
-              onClick={() => onCopy()}
-            >
-              {hasCopied ? t('Referal link copied') : t('Refer-a-Friend')}
-            </Button>
-          </Box>
-        )}
-      </Card>
-      <Card my="8" borderRadius="2xl !important">
-        <Box m="auto" maxW={isSmallScreen ? '600px' : '100%'}>
-          <Box m="auto" position="relative" w="300px">
-            <Image w="300px" src="/images/bankless-score.png" />
-            <Box
-              position="absolute"
-              top="55px"
-              width="72px"
-              textAlign="center"
-              left="212px"
-              fontSize="5xl"
-              fontWeight="bold"
-            >
-              {user.stats.score || 0}
-            </Box>
-          </Box>
-          <Box display={isSmallScreen ? 'block' : 'flex'} m="8">
-            <Box
-              w={isSmallScreen ? '100%' : '50%'}
-              mr={isSmallScreen ? '0' : '50px'}
-            >
-              <ProgressTitle
-                title={t('Badges')}
-                score={user.stats.badges || 0}
-                max={9}
-                description={t(
-                  `Each lesson badges is going to increase your Bankless Explorer score by 1 point.`
-                )}
-              />
-              <Badges
-                badges={user.badgeTokenIds}
-                badgeToHighlight={badgeToHighlight}
-                type="badges"
-                isMyProfile={isMyProfile}
-              />
-            </Box>
-            <Box w={isSmallScreen ? '100%' : '50%'}>
-              <ProgressTitle
-                title={t('Collectibles')}
-                score={
-                  3 * (user.stats?.datadisks?.length || 0) +
-                  (user.stats?.handbooks?.length || 0)
-                }
-                max={8}
-                description={t(
-                  `1 DATADISK will get you 3 points and 1 HANDBOOK is equivalent to 1 point.`
-                )}
-              />
-              <Badges
-                badges={collectibles}
-                type="collectibles"
-                isMyProfile={isMyProfile}
-              />
-            </Box>
-          </Box>
+  if (user)
+    return (
+      <Container maxW="container.lg">
+        <Card mt="180px" borderRadius="2xl !important">
           <Box
-            display={isSmallScreen ? 'block' : 'flex'}
-            m="8"
-            maxW={isSmallScreen ? '600px' : '100%'}
+            margin="auto"
+            mt="-130px"
+            pt="12px"
+            w="284px"
+            h="284px"
+            borderRadius="50%"
+            backgroundImage="linear-gradient(180deg, #A379BD 0%, #5B5198 100%)"
           >
-            <Box
-              w={isSmallScreen ? '100%' : '50%'}
-              mr={isSmallScreen ? '0' : '50px'}
-            >
-              <ProgressTitle
-                title={t('Donations')}
-                score={
-                  user.stats?.donations
-                    ? Object.keys(user.stats?.donations)?.length || 0
-                    : 0
-                }
-                max={8}
-                description={t(
-                  `Each time you donate to Bankless Academy via Gitcoin, you increase your score by 1 point. Donation score are only updated after the end of a round.`
-                )}
-              />
-              <Badges
-                badges={Object.keys(user.donations)}
-                type="donations"
-                isMyProfile={isMyProfile}
-              />
+            <Image
+              w="260px"
+              h="260px"
+              margin="auto"
+              borderRadius="50%"
+              backgroundColor="black"
+              src={user.avatar}
+            />
+          </Box>
+          <Text
+            as="h2"
+            fontSize="4xl"
+            fontWeight="bold"
+            textAlign="center"
+            textTransform="uppercase"
+            mt="40px"
+            mb="8"
+          >
+            {user.ensName || shortenAddress(profileAddress)}
+          </Text>
+          {isMyProfile && (
+            <Box justifyContent="center" w="256px" m="auto" mb="8">
+              <Box pb="2">
+                <ExternalLink href={twitterLink} mr="2">
+                  <Button
+                    variant="primary"
+                    w="100%"
+                    borderBottomRadius="0"
+                    leftIcon={<Image width="24px" src="/images/TwitterX.svg" />}
+                  >
+                    {t('Share on Twitter')}
+                  </Button>
+                </ExternalLink>
+              </Box>
+              <Button
+                variant="primary"
+                w="100%"
+                borderTopRadius="0"
+                leftIcon={<UserPlus size="30px" />}
+                onClick={() => onCopy()}
+              >
+                {hasCopied ? t('Referal link copied') : t('Refer-a-Friend')}
+              </Button>
             </Box>
-            <Box w={isSmallScreen ? '100%' : '50%'}>
-              <ProgressTitle
-                title={t('Stamps')}
-                score={user.stats?.valid_stamps?.length}
-                max={8}
-                description={t(
-                  `Each Gitcoin Passport stamp is going to increase you Bankless Explorer score by 1 point.`
-                )}
-              />
-              <Badges
-                badges={user.stats?.valid_stamps}
-                type="stamps"
-                isMyProfile={isMyProfile}
-              />
+          )}
+        </Card>
+        <Card my="8" borderRadius="2xl !important">
+          <Box m="auto" maxW={isSmallScreen ? '600px' : '100%'}>
+            <Box m="auto" position="relative" w="300px">
+              <Image w="300px" src="/images/bankless-score.png" />
+              <Box
+                position="absolute"
+                top="55px"
+                width="72px"
+                textAlign="center"
+                left="212px"
+                fontSize="5xl"
+                fontWeight="bold"
+              >
+                {user.stats.score || 0}
+              </Box>
+            </Box>
+            <Box display={isSmallScreen ? 'block' : 'flex'} m="8">
+              <Box
+                w={isSmallScreen ? '100%' : '50%'}
+                mr={isSmallScreen ? '0' : '50px'}
+              >
+                <ProgressTitle
+                  title={t('Badges')}
+                  score={user.stats.badges || 0}
+                  max={9}
+                  description={t(
+                    `Each lesson badges is going to increase your Bankless Explorer score by 1 point.`
+                  )}
+                />
+                <Badges
+                  badges={user.badgeTokenIds}
+                  badgeToHighlight={badgeToHighlight}
+                  type="badges"
+                  isMyProfile={isMyProfile}
+                />
+              </Box>
+              <Box w={isSmallScreen ? '100%' : '50%'}>
+                <ProgressTitle
+                  title={t('Collectibles')}
+                  score={
+                    3 * (user.stats?.datadisks?.length || 0) +
+                    (user.stats?.handbooks?.length || 0)
+                  }
+                  max={8}
+                  description={t(
+                    `1 DATADISK will get you 3 points and 1 HANDBOOK is equivalent to 1 point.`
+                  )}
+                />
+                <Badges
+                  badges={collectibles}
+                  type="collectibles"
+                  isMyProfile={isMyProfile}
+                />
+              </Box>
+            </Box>
+            <Box
+              display={isSmallScreen ? 'block' : 'flex'}
+              m="8"
+              maxW={isSmallScreen ? '600px' : '100%'}
+            >
+              <Box
+                w={isSmallScreen ? '100%' : '50%'}
+                mr={isSmallScreen ? '0' : '50px'}
+              >
+                <ProgressTitle
+                  title={t('Donations')}
+                  score={
+                    user.stats?.donations
+                      ? Object.keys(user.stats?.donations)?.length || 0
+                      : 0
+                  }
+                  max={8}
+                  description={t(
+                    `Each time you donate to Bankless Academy via Gitcoin, you increase your score by 1 point. Donation score are only updated after the end of a round.`
+                  )}
+                />
+                <Badges
+                  badges={Object.keys(user.donations)}
+                  type="donations"
+                  isMyProfile={isMyProfile}
+                />
+              </Box>
+              <Box w={isSmallScreen ? '100%' : '50%'}>
+                <ProgressTitle
+                  title={t('Stamps')}
+                  score={user.stats?.valid_stamps?.length}
+                  max={8}
+                  description={t(
+                    `Each Gitcoin Passport stamp is going to increase you Bankless Explorer score by 1 point.`
+                  )}
+                />
+                <Badges
+                  badges={user.stats?.valid_stamps}
+                  type="stamps"
+                  isMyProfile={isMyProfile}
+                />
+              </Box>
             </Box>
           </Box>
-        </Box>
-      </Card>
-    </Container>
-  )
+        </Card>
+      </Container>
+    )
+  else
+    return (
+      <Container maxW="container.xl">
+        <Heading as="h2" size="xl" m="8" textAlign="center">
+          Loading Explorer Profile
+        </Heading>
+        {error || (
+          <Image
+            margin="auto"
+            paddingTop="200px"
+            width="250px"
+            src="/loading_purple.svg"
+          />
+        )}
+      </Container>
+    )
 }
