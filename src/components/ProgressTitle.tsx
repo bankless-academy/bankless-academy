ca9e0146950aade05aefae15d3def0d0134438ad
Didier Krux <didier@krux.co>
2023-12-08 17:29:11 +0000

    improve ProgressTitle style

--- src/components/ProgressTitle.tsx
+++ src/components/ProgressTitle.tsx
@@ -1,6 +1,7 @@
 import { Box, Text } from '@chakra-ui/react'
 import { ReactNode } from 'react'
 import Helper from 'components/Helper'
+import { useSmallScreen } from 'hooks'
 
 const ProgressTitle = ({
   title,
@@ -12,54 +13,57 @@ const ProgressTitle = ({
   score: number
   max: number
   description: ReactNode
-}): React.ReactElement => (
-  <Box
-    borderBottom="1px solid #989898"
-    display="flex"
-    position="relative"
-    fontSize="2xl"
-    fontWeight="bold"
-    justifyContent="flex-end"
-    pb="4"
-    mb="8"
-  >
-    <Box mr="4">{score}</Box>
+}): React.ReactElement => {
+  const [, , isSmallScreen] = useSmallScreen()
+  return (
     <Box
-      width={`${(score / max) * 200}px`}
-      borderLeftRadius="2px"
-      background="linear-gradient(223deg, #3a355a 16.65%, #634c70 95.78%)"
-    />
-    <Text
-      as="h2"
-      borderRightRadius="2px"
-      border="2px solid #8a68a2"
-      borderLeft="0"
-      width="200px"
-      backgroundColor="transparent"
-      textTransform="uppercase"
-      textAlign="end"
-      pr="2"
-    >
-      {title}
-    </Text>
-    <Box
+      borderBottom="1px solid #989898"
       display="flex"
-      position="absolute"
-      top="-5px"
-      height="50px"
-      right="199px"
-      borderRight="1px #989898 solid"
-    />
-    <Helper
-      title={title}
-      isProfile={true}
-      definition={
-        <>
-          <Box>{description}</Box>
-        </>
-      }
-    />
-  </Box>
-)
+      position="relative"
+      fontSize="2xl"
+      fontWeight="bold"
+      justifyContent="flex-end"
+      pb="4"
+      mb="8"
+    >
+      <Box mr="4" my="4px">
+        {score}
+      </Box>
+      <Box
+        width={`${(score / max) * 200}px`}
+        borderLeftRadius="2px"
+        background="linear-gradient(223deg, #3a355a 16.65%, #634c70 95.78%)"
+        my="4px"
+      />
+      <Box display="flex" height="50px" borderRight="1px #989898 solid" />
+      <Text
+        as="h2"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        borderRightRadius="2px"
+        border="2px solid #8a68a2"
+        borderLeft="0"
+        width={isSmallScreen ? '140px' : '200px'}
+        fontSize={isSmallScreen ? 'md' : 'inherit'}
+        backgroundColor="transparent"
+        textTransform="uppercase"
+        pb="1px"
+        my="4px"
+      >
+        {title}
+      </Text>
+      <Helper
+        title={title}
+        isProfile={true}
+        definition={
+          <>
+            <Box>{description}</Box>
+          </>
+        }
+      />
+    </Box>
+  )
+}
 
 export default ProgressTitle
