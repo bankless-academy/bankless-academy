6ba001dbfe6d926413dd9ad451499bb5db725dd4
Didier Krux <didier@krux.co>
2023-12-08 18:25:36 +0000

    updateENS api

--- src/pages/api/cache/[cache].ts
+++ src/pages/api/cache/[cache].ts
@@ -15,7 +15,8 @@ export default async function handler(req: NextRequest) {
   const cache = req.nextUrl.pathname.split('/')[3]
   // console.log(cache)
   if (cache && (AUTHORIZED_KV.includes(cache) || cache === 'top1000_leaderboard')) {
-    const data = await kv.get(cache)
+    const data: any = await kv.get(cache)
+    // console.log(Object.keys(data.data))
     return new NextResponse(JSON.stringify(data), {
       status: 200,
     })
