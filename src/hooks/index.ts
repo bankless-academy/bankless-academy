a9a11d7c399c3f3e1ecad0277a19bf952dd65eb7
Didier Krux <didier@krux.co>
2023-12-08 17:28:17 +0000

    add small screen media query

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,5 @@
 import { useMediaQuery } from '@chakra-ui/react'
 
 export function useSmallScreen(): boolean[] {
-  return useMediaQuery(['(max-width: 800px)', '(max-width: 1260px)'])
+  return useMediaQuery(['(max-width: 800px)', '(max-width: 1260px)', '(max-width: 500px)'])
 }
