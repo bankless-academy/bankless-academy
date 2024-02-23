7587d6ec72bba66ef40b9bc468e1e87a26034c5d
Didier Krux <didier@krux.co>
2024-02-23 11:24:56 +0000

    improve report form

--- src/pages/report-an-issue.tsx
+++ src/pages/report-an-issue.tsx
@@ -6,6 +6,7 @@ import { useAccount } from 'wagmi'
 import { MetaData } from 'components/Head'
 import { useSmallScreen } from 'hooks/index'
 import { DOMAIN_PROD } from 'constants/index'
+import { useRouter } from 'next/router'
 
 const pageMeta: MetaData = {
   title: 'Report an Issue',
@@ -19,13 +20,17 @@ export const getStaticProps: GetStaticProps = async () => {
 
 const ReportAnIssue = (): JSX.Element => {
   const [isSmallScreen] = useSmallScreen()
+  const router = useRouter()
   const { address } = useAccount()
+  const { context } = router.query
 
   return (
     <Box height={isSmallScreen ? 'calc(100vh - 64px)' : 'calc(100vh - 73px)'}>
       <Box w="100%" h="100%" padding="0" backgroundColor="transparent">
         <iframe
-          src={`https://tally.so/embed/wAr8gz?hideTitle=0&alignLeft=1&wallet=${address}&domain=${DOMAIN_PROD}`}
+          src={`https://tally.so/embed/wAr8gz?hideTitle=0&alignLeft=1&wallet=${address}&domain=${DOMAIN_PROD}&context=${encodeURIComponent(
+            (context as string) || document?.referrer || 'no_context'
+          )}`}
           frameBorder="0"
           width="100%"
           height="100%"
