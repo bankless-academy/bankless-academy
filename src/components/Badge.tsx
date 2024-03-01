de07560e28bf4a39d185e57ae85275c323de594f
Didier Krux <didier@krux.co>
2024-03-01 20:54:16 +0000

    fix wallet not connected

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -174,13 +174,13 @@ Join the journey and level up your #web3 knowledge! 👨‍🚀🚀`
                       w="100%"
                       height="100%"
                       onClick={() => {
-                        console.log('dd')
                         if (!isQuestCompleted) {
                           setTriggerOpen(true)
                           setTimeout(() => {
                             setTriggerOpen(false)
                           }, 100)
-                        } else if (!address) {
+                        }
+                        if (!address) {
                           setConnectWalletPopupLS(true)
                         }
                       }}
