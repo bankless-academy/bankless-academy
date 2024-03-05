558b0f69f83113ac803fd02ec64dbd80d435fbd3
Didier Krux <didier@krux.co>
2024-03-05 09:14:57 +0000

    dynamic datadisk metadata

--- src/pages/api/metadata/[...slug].ts
+++ src/pages/api/metadata/[...slug].ts
@@ -14,8 +14,10 @@ export default async function handler(
     slug: [slug, tokenId],
   } = req.query
 
+  // TODO: save assets on Arweave
+
+  // Badge contract
   if (slug === 'badge-contrat-uri') {
-    // https://beta.banklessacademy.com/api/metadata/badge-contrat-uri
     // https://app.banklessacademy.com/api/metadata/badge-contrat-uri
     const metadata = {
       name: "Bankless Academy Badges",
@@ -28,25 +30,44 @@ export default async function handler(
 
   if (!slug || !tokenId) return res.status(400).json({ error: 'Wrong params' })
 
-  if (!['badge', 'badgev2'].includes(slug))
-    return res.status(400).json({ error: 'Unknown contract' })
-  const lesson = LESSONS.find(
-    (lesson) => lesson.badgeId === parseInt(tokenId, 16)
-  )
-
-  if (!lesson) return res.status(400).json({ error: 'Unknown tokenId' })
-
-  // https://beta.banklessacademy.com/api/metadata/badge/{id}
-  // https://app.banklessacademy.com/api/metadata/badge/{id}
-  const metadata = {
-    name: `${lesson.name}${slug === 'badgev2' ? ' v2' : ''}`,
-    description: `${lesson.description} ${lessonAddress(lesson)}`,
-    external_url: lessonAddress(lesson),
-    image: `https://${DOMAIN_PROD}${lesson.badgeImageLink}`,
-    attributes: [
-      { trait_type: 'created_by', value: 'Bankless Academy' },
-      { trait_type: 'links', value: [lessonAddress(lesson)] },
-    ],
+  if (['badge', 'badgev2'].includes(slug)) {
+    // Badges
+    const lesson = LESSONS.find(
+      (lesson) => lesson.badgeId === parseInt(tokenId, 16)
+    )
+    if (!lesson) return res.status(400).json({ error: 'Unknown tokenId' })
+    // https://app.banklessacademy.com/api/metadata/badge/{id}
+    // https://app.banklessacademy.com/api/metadata/badge/0000000000000000000000000000000000000000000000000000000000000001
+    const metadata = {
+      name: `${lesson.name}${slug === 'badgev2' ? ' v2' : ''}`,
+      description: `${lesson.description} ${lessonAddress(lesson)}`,
+      image: `https://${DOMAIN_PROD}${lesson.badgeImageLink}`,
+      attributes: [
+        { trait_type: 'created_by', value: 'Bankless Academy' },
+      ],
+      external_url: lessonAddress(lesson),
+    }
+    return res.status(200).json(metadata)
+  } else if (['datadisk'].includes(slug)) {
+    // DataDisks
+    const lesson = LESSONS.find(
+      (lesson) => lesson.lessonCollectibleTokenAddress === tokenId
+    )
+    if (!lesson) return res.status(400).json({ error: 'Unknown tokenId' })
+    // https://app.banklessacademy.com/api/metadata/datadisk/{nftContractAddress}
+    // https://app.banklessacademy.com/api/metadata/datadisk/0x92B55D5254bC93A4f282224A9C3bD2b7e0eF37fc
+    const metadata = {
+      name: `${lesson.name}`,
+      description: `${lesson.description} ${lessonAddress(lesson)}`,
+      image: `https://${DOMAIN_PROD}${lesson.lessonCollectedImageLink}`,
+      animation_url: `https://${DOMAIN_PROD}${lesson.lessonCollectibleGif}`,
+      attributes: [
+        { trait_type: 'created_by', value: 'Bankless Academy' },
+      ],
+      external_url: lessonAddress(lesson),
+    }
+    return res.status(200).json(metadata)
   }
-  return res.status(200).json(metadata)
+
+  return res.status(400).json({ error: 'Unknown contract' })
 }
