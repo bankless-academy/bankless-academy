cd04423f96047247bb723fbde114c0d95e2e5cce
Didier Krux <didier@krux.co>
2024-03-19 17:25:21 +0000

    dyn mirror contract

--- src/components/CollectEntryButton.tsx
+++ src/components/CollectEntryButton.tsx
@@ -42,56 +42,56 @@ const CollectEntryButton = ({
   )
   const [showConfetti, setShowConfetti] = useState(false)
 
+  const isNewContract = parseInt(lesson?.collectibleId.substring(1), 10) > 6
+
   const { config } = usePrepareContractWrite({
     address: lesson.mirrorNFTAddress,
-    abi:
-      lesson.mirrorNFTAddress === '0xe2dd5eb9841920937833c094874ba33d26becb4f'
-        ? [
-            {
-              inputs: [
-                {
-                  internalType: 'address',
-                  name: 'tokenRecipient',
-                  type: 'address',
-                },
-                { internalType: 'string', name: 'message', type: 'string' },
-                {
-                  internalType: 'address',
-                  name: 'mintReferral',
-                  type: 'address',
-                },
-              ],
-              name: 'purchase',
-              outputs: [
-                { internalType: 'uint256', name: 'tokenId', type: 'uint256' },
-              ],
-              stateMutability: 'payable',
-              type: 'function',
-            },
-          ]
-        : [
-            {
-              inputs: [
-                {
-                  internalType: 'address',
-                  name: 'tokenRecipient',
-                  type: 'address',
-                },
-                { internalType: 'string', name: 'message', type: 'string' },
-              ],
-              name: 'purchase',
-              outputs: [
-                { internalType: 'uint256', name: 'tokenId', type: 'uint256' },
-              ],
-              stateMutability: 'payable',
-              type: 'function',
-            },
-          ],
+    abi: isNewContract
+      ? [
+          {
+            inputs: [
+              {
+                internalType: 'address',
+                name: 'tokenRecipient',
+                type: 'address',
+              },
+              { internalType: 'string', name: 'message', type: 'string' },
+              {
+                internalType: 'address',
+                name: 'mintReferral',
+                type: 'address',
+              },
+            ],
+            name: 'purchase',
+            outputs: [
+              { internalType: 'uint256', name: 'tokenId', type: 'uint256' },
+            ],
+            stateMutability: 'payable',
+            type: 'function',
+          },
+        ]
+      : [
+          {
+            inputs: [
+              {
+                internalType: 'address',
+                name: 'tokenRecipient',
+                type: 'address',
+              },
+              { internalType: 'string', name: 'message', type: 'string' },
+            ],
+            name: 'purchase',
+            outputs: [
+              { internalType: 'uint256', name: 'tokenId', type: 'uint256' },
+            ],
+            stateMutability: 'payable',
+            type: 'function',
+          },
+        ],
     functionName: 'purchase',
-    args:
-      lesson.mirrorNFTAddress === '0xe2dd5eb9841920937833c094874ba33d26becb4f'
-        ? [address, '', '0x0000000000000000000000000000000000000000']
-        : [address, ''],
+    args: isNewContract
+      ? [address, '', '0x0000000000000000000000000000000000000000']
+      : [address, ''],
     chainId: optimism.id,
     // 0.01 + 0.00069 in collector fee
     value: parseEther('0.01069'),
