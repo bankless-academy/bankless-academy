d54b4b2227bea5551773edb62716cf5335d51a8b
Didier Krux <didier@krux.co>
2024-03-27 18:01:50 +0000

    update translation + language config

--- src/entities/lesson.ts
+++ src/entities/lesson.ts
@@ -1,7 +1,7 @@
 import { QuestComponentType } from 'components/Quest/QuestComponent'
 
 export type SlideType = 'LEARN' | 'QUIZ' | 'QUEST' | 'END'
-export type LanguageType = 'en' | 'cn' | 'de' | 'es' | 'fr' | 'it' | 'jp' | 'br' | 'ua'
+export type LanguageType = 'en' | 'br' | 'cn' | 'de' | 'es' | 'fr' | 'it' | 'jp' | 'tr' | 'ua'
 
 export interface LessonType {
   name: string
--- src/utils/translation.ts
+++ src/utils/translation.ts
@@ -2,53 +2,74 @@ import i18next from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
 
-// EN
+// EN = English
 import common from '../../translation/website/en/common.json'
 import homepage from '../../translation/website/en/homepage.json'
 import quests from '../../translation/website/en/quests.json'
 import keywords from '../../translation/keywords/en/keywords.json'
 
-// CN
+// BR = Brazilian Portuguese
+import commonBR from '../../translation/website/br/common.json'
+// import homepageBR from '../../translation/website/br/homepage.json'
+import questsBR from '../../translation/website/br/quests.json'
+import keywordsBR from '../../translation/keywords/br/keywords.json'
+import lessonBR from '../../translation/website/br/lesson.json'
+
+// CN = Chinese Simplified
 import commonCN from '../../translation/website/cn/common.json'
 // import homepageCN from '../../translation/website/cn/homepage.json'
 import questsCN from '../../translation/website/cn/quests.json'
 import keywordsCN from '../../translation/keywords/cn/keywords.json'
 import lessonCN from '../../translation/website/cn/lesson.json'
 
-// DE
+// DE = German
 import commonDE from '../../translation/website/de/common.json'
 // import homepageDE from '../../translation/website/de/homepage.json'
 import questsDE from '../../translation/website/de/quests.json'
 import keywordsDE from '../../translation/keywords/de/keywords.json'
 import lessonDE from '../../translation/website/de/lesson.json'
 
-// ES
+// ES = Spanish
 import commonES from '../../translation/website/es/common.json'
 // import homepageES from '../../translation/website/es/homepage.json'
 import questsES from '../../translation/website/es/quests.json'
 import keywordsES from '../../translation/keywords/es/keywords.json'
 import lessonES from '../../translation/website/es/lesson.json'
 
-// FR
+// FR = French
 import commonFR from '../../translation/website/fr/common.json'
 import homepageFR from '../../translation/website/fr/homepage.json'
 import questsFR from '../../translation/website/fr/quests.json'
 import keywordsFR from '../../translation/keywords/fr/keywords.json'
 import lessonFR from '../../translation/website/fr/lesson.json'
 
-// IT
+// IT = Italian
 import commonIT from '../../translation/website/it/common.json'
 // import homepageIT from '../../translation/website/it/homepage.json'
 import questsIT from '../../translation/website/it/quests.json'
 import keywordsIT from '../../translation/keywords/it/keywords.json'
 import lessonIT from '../../translation/website/it/lesson.json'
 
-// JP
+// JP = Japanese
 import commonJP from '../../translation/website/jp/common.json'
 // import homepageJP from '../../translation/website/jp/homepage.json'
 import questsJP from '../../translation/website/jp/quests.json'
 import keywordsJP from '../../translation/keywords/jp/keywords.json'
 import lessonJP from '../../translation/website/jp/lesson.json'
+
+// TR = Turkish
+import commonTR from '../../translation/website/tr/common.json'
+// import homepageTR from '../../translation/website/tr/homepage.json'
+import questsTR from '../../translation/website/tr/quests.json'
+import keywordsTR from '../../translation/keywords/tr/keywords.json'
+import lessonTR from '../../translation/website/tr/lesson.json'
+
+// UA = Ukrainian
+import commonUA from '../../translation/website/ua/common.json'
+// import homepageUA from '../../translation/website/ua/homepage.json'
+import questsUA from '../../translation/website/ua/quests.json'
+import keywordsUA from '../../translation/keywords/ua/keywords.json'
+import lessonUA from '../../translation/website/ua/lesson.json'
 
 export const defaultNS = 'common'
 
@@ -64,6 +85,13 @@ i18next
         homepage,
         quests,
         keywords,
+      },
+      br: {
+        common: commonBR,
+        // homepage: homepageBR,
+        quests: questsBR,
+        keywords: keywordsBR,
+        lesson: lessonBR,
       },
       cn: {
         common: commonCN,
@@ -106,6 +134,20 @@ i18next
         quests: questsJP,
         keywords: keywordsJP,
         lesson: lessonJP,
+      },
+      tr: {
+        common: commonTR,
+        // homepage: homepageTR,
+        quests: questsTR,
+        keywords: keywordsTR,
+        lesson: lessonTR,
+      },
+      ua: {
+        common: commonUA,
+        // homepage: homepageUA,
+        quests: questsUA,
+        keywords: keywordsUA,
+        lesson: lessonUA,
       }
     },
     defaultNS,
