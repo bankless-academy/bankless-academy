50811470899f7f51a9173b2cb3cf9c8c3b52e538
Didier Krux <didier@krux.co>
2024-04-11 13:28:03 +0100

    generateKeywords cmd option

--- import-content.js
+++ import-content.js
@@ -166,6 +166,7 @@ program
   .option('-nid, --notionID <type>', 'specify Notion ID of the database', process.env.DEFAULT_CONTENT_DB_ID || DEFAULT_NOTION_ID)
   .option('-lid, --lessonID <type>', 'specify Lesson Notion ID')
   .option('-lg, --translate <type>', 'specific language to translate (fr, es, ...) or all')
+  .option('-gk, --generateKeywords <type>', 'generate keyword translation files for each language')
 
 program.parse(process.argv)
 const NOTION_ID = program.opts().notionID
@@ -675,7 +676,7 @@ axios
             return slide
           })
           lesson.keywords = allKeywords
-          if (LESSON_NOTION_ID) {
+          if (LESSON_NOTION_ID && program.opts().generateKeywords) {
             // create keyword lesson file
             const lessonKV = {}
             for (const keyword of allKeywords) {
