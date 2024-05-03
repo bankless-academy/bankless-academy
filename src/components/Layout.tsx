3594105b5a0b6bba8fbec9b81c03b495b6631e55
Didier Krux <didier@krux.co>
2024-05-03 18:46:55 +0100

    update layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
-import { Box, Container, Button, Image } from '@chakra-ui/react'
+import { Box, Container, Image, Icon } from '@chakra-ui/react'
 import { ReactElement } from 'react'
+import { BookOpen, Icon as PhosphorIcon } from '@phosphor-icons/react'
 
 import { LessonTypeType } from 'entities/lesson'
 import InternalLink from './InternalLink'
@@ -9,12 +10,86 @@ import { useAccount } from 'wagmi'
 import { useLocalStorage } from 'usehooks-ts'
 import { shortenAddress } from 'utils/index'
 
+export type PageType = LessonTypeType | 'PROFILE'
+
+const DesktopButton = ({
+  isActive,
+  link,
+  label,
+  icon,
+}: {
+  isActive: boolean
+  link: string
+  label: string
+  icon: PhosphorIcon
+}): React.ReactElement => {
+  const button = (
+    <Box w="100%" h="100%" display="flex" alignItems="center" color="white">
+      <Box ml="4" display="flex">
+        <Icon as={icon} />
+      </Box>
+      <Box ml="4" fontSize="xl" fontWeight="semibold">
+        {label}
+      </Box>
+    </Box>
+  )
+  return (
+    <Box
+      h="64px"
+      mt="2"
+      background={
+        isActive
+          ? 'linear-gradient(132deg, #67407E 0%, #354374 100%)'
+          : '#3F3253'
+      }
+      border={isActive ? '2px solid #B85FF1' : ''}
+    >
+      {isActive ? button : <InternalLink href={link}>{button}</InternalLink>}
+    </Box>
+  )
+}
+
+const MobileButton = ({
+  isActive,
+  link,
+  label,
+  icon,
+}: {
+  isActive: boolean
+  link: string
+  label: string
+  icon: PhosphorIcon
+}): React.ReactElement => {
+  const button = (
+    <Box
+      borderRadius="50%"
+      w="56px"
+      h="56px"
+      background={
+        isActive
+          ? 'linear-gradient(132deg, #67407E 0%, #354374 100%)'
+          : '#3F3253'
+      }
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Icon alt={label} as={icon} />
+    </Box>
+  )
+  return (
+    <Box mt="2px">
+      {isActive ? button : <InternalLink href={link}>{button}</InternalLink>}
+    </Box>
+  )
+}
+
 const Layout = ({
   children,
   page,
 }: {
   children: ReactElement
-  page?: LessonTypeType | 'PROFILE'
+  page?: PageType
 }): React.ReactElement => {
   const { address } = useAccount()
   const [nameCache] = useLocalStorage(`name-cache`, {})
@@ -53,12 +128,20 @@ const Layout = ({
         <>
           {/* Desktop menu */}
           <Box
-            w={menuBarWidth}
+            w={`calc(${menuBarWidth} + 2px)`}
             position={scrollY > 80 ? 'fixed' : 'absolute'}
             top="0"
+            mt="2"
           >
             {address && (
-              <>
+              <Box
+                pt="2"
+                background={
+                  page === 'PROFILE'
+                    ? 'linear-gradient(132deg, #67407E 0%, #354374 100%)'
+                    : '#3F3253'
+                }
+              >
                 <InternalLink href={`/explorer/${username}?referral=true`}>
                   <Box
                     margin="auto"
@@ -78,31 +161,27 @@ const Layout = ({
                       src={`https://ensdata.net/media/avatar/${username}`}
                     />
                   </Box>
-                  <Box textAlign="center" mt="4">
+                  <Box textAlign="center" mt="4" color="white">
                     {username.includes('.')
                       ? username
                       : shortenAddress(username)}
                   </Box>
                 </InternalLink>
-              </>
+              </Box>
             )}
-            <Box pl="4">
-              <Box mt="4">
-                <InternalLink href="/lessons">
-                  <Button variant={page === 'LESSON' ? 'primary' : 'secondary'}>
-                    Lesson Select
-                  </Button>
-                </InternalLink>
-              </Box>
-              <Box mt="4">
-                <InternalLink href="/lessons/handbook">
-                  <Button
-                    variant={page === 'HANDBOOK' ? 'primary' : 'secondary'}
-                  >
-                    Explorer’s Handbook
-                  </Button>
-                </InternalLink>
-              </Box>
+            <Box>
+              <DesktopButton
+                link="/lessons"
+                label="Lesson Select"
+                isActive={page === 'LESSON'}
+                icon={BookOpen}
+              />
+              <DesktopButton
+                link="/lessons/handbook"
+                label="Explorer’s Handbook"
+                isActive={page === 'HANDBOOK'}
+                icon={BookOpen}
+              />
             </Box>
           </Box>
         </>
@@ -119,27 +198,24 @@ const Layout = ({
           placeItems="center"
         >
           {/* Mobile menu */}
-          <Box>
-            <InternalLink href={`/explorer/${username}?referral=true`}>
-              <Button variant={page === 'PROFILE' ? 'primary' : 'secondary'}>
-                Profile
-              </Button>
-            </InternalLink>
-          </Box>
-          <Box>
-            <InternalLink href="/lessons">
-              <Button variant={page === 'LESSON' ? 'primary' : 'secondary'}>
-                Essentials
-              </Button>
-            </InternalLink>
-          </Box>
-          <Box>
-            <InternalLink href="/lessons/handbook">
-              <Button variant={page === 'HANDBOOK' ? 'primary' : 'secondary'}>
-                Handbook
-              </Button>
-            </InternalLink>
-          </Box>
+          <MobileButton
+            link={`/explorer/${username}?referral=true`}
+            label="Profile"
+            isActive={page === 'PROFILE'}
+            icon={BookOpen}
+          />
+          <MobileButton
+            link="/lessons"
+            label="Essentials"
+            isActive={page === 'LESSON'}
+            icon={BookOpen}
+          />
+          <MobileButton
+            link="/lessons/handbook"
+            label="Handbook"
+            isActive={page === 'HANDBOOK'}
+            icon={BookOpen}
+          />
         </Box>
       )}
     </Box>
