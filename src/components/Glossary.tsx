5b4040289447e5bc37524218a30c9c3a5b285625
Didier Krux <didier@krux.co>
2024-05-13 18:23:33 +0100

    fix sizing

--- src/components/Glossary.tsx
+++ src/components/Glossary.tsx
@@ -26,13 +26,10 @@ const Glossary: React.FC<GlossaryProps> = ({ terms }) => {
 
   return (
     <Box position="relative">
-      <StyledHeading as="h1" size="2xl" textAlign="center" my={8}>
-        Glossary
-      </StyledHeading>
       <Box
         position="fixed"
         top="80px"
-        right="20px"
+        right="18px"
         h="calc(100% - 140px)"
         display="grid"
         overflow="scroll"
@@ -44,7 +41,10 @@ const Glossary: React.FC<GlossaryProps> = ({ terms }) => {
           </a>
         ))}
       </Box>
-      <Box m="10">
+      <Box m="10" pr="2" maxW="1024px">
+        <StyledHeading as="h1" size="2xl" textAlign="center" my={8}>
+          Glossary
+        </StyledHeading>
         {Object.keys(groupedTerms).map((letter) => (
           <Box key={letter} id={`section-${letter}`}>
             <Box
