d05c74ee7f819e56a557beca948c52efb8e2d5b1
Didier Krux <didier@krux.co>
2024-06-21 19:08:01 +0100

    add AA sponsored tx support to MintNFT

--- src/components/MintNFT.tsx
+++ src/components/MintNFT.tsx
@@ -1,20 +1,24 @@
 /* eslint-disable no-console */
 import { Box, Button, Image, useToast } from '@chakra-ui/react'
-import { useEffect, useState } from 'react'
-import { switchChain } from '@wagmi/core'
-import { baseSepolia } from 'wagmi/chains'
+import { useEffect, useMemo, useState } from 'react'
+import { switchChain, simulateContract, writeContract } from '@wagmi/core'
+import { base } from 'wagmi/chains'
 import { useWaitForTransactionReceipt, useAccount } from 'wagmi'
-import { simulateContract, writeContract } from '@wagmi/core'
 import { Gear, SealCheck } from '@phosphor-icons/react'
 import { useWeb3Modal } from '@web3modal/wagmi/react'
+import { useCapabilities, useWriteContracts } from 'wagmi/experimental'
 
 import ExternalLink from 'components/ExternalLink'
 import { useSmallScreen } from 'hooks/index'
 import Confetti from 'components/Confetti'
 import { wagmiConfig } from 'utils/wagmi'
+import { NFTAddress, nftABI } from 'constants/nft'
+import { api, getNFTsCollectors } from 'utils/index'
+import { client } from 'utils/paymaster'
 
 const MintNFT = (): JSX.Element => {
   const { address, chain } = useAccount()
+  const account = useAccount()
   const { open } = useWeb3Modal()
   const toast = useToast()
   const [isSmallScreen] = useSmallScreen()
@@ -23,63 +27,35 @@ const MintNFT = (): JSX.Element => {
   const [hash, setHash] = useState('')
   const [mintingError, setMintingError] = useState('')
   const [showConfetti, setShowConfetti] = useState(false)
+  const { writeContracts } = useWriteContracts({
+    mutation: {
+      onSuccess: (id) => {
+        celebrateMint()
+        console.log('id', id)
+      },
+    },
+  })
 
-  // TEMP
-  const numberCollected = '5'
-
-  const NFTAddress = '0x7549f01633aebccddabcaa0f8046a12d8baddeee'
+  const contractArgs = [
+    address,
+    '1',
+    '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE',
+    '0',
+    [
+      ['0x0000000000000000000000000000000000000000000000000000000000000000'],
+      '1',
+      '0',
+      '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE',
+    ],
+    '0x',
+  ]
 
   const mintArg = {
     address: NFTAddress,
-    abi: [
-      {
-        inputs: [
-          { internalType: 'address', name: '_receiver', type: 'address' },
-          { internalType: 'uint256', name: '_quantity', type: 'uint256' },
-          { internalType: 'address', name: '_currency', type: 'address' },
-          { internalType: 'uint256', name: '_pricePerToken', type: 'uint256' },
-          {
-            components: [
-              { internalType: 'bytes32[]', name: 'proof', type: 'bytes32[]' },
-              {
-                internalType: 'uint256',
-                name: 'quantityLimitPerWallet',
-                type: 'uint256',
-              },
-              {
-                internalType: 'uint256',
-                name: 'pricePerToken',
-                type: 'uint256',
-              },
-              { internalType: 'address', name: 'currency', type: 'address' },
-            ],
-            internalType: 'struct IDrop.AllowlistProof',
-            name: '_allowlistProof',
-            type: 'tuple',
-          },
-          { internalType: 'bytes', name: '_data', type: 'bytes' },
-        ],
-        name: 'claim',
-        outputs: [],
-        stateMutability: 'payable',
-        type: 'function',
-      },
-    ],
+    abi: nftABI,
     functionName: 'claim',
-    args: [
-      address,
-      '1',
-      '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE',
-      '0',
-      [
-        ['0x0000000000000000000000000000000000000000000000000000000000000000'],
-        '1',
-        '0',
-        '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE',
-      ],
-      '0x',
-    ],
-    chainId: baseSepolia.id,
+    args: contractArgs,
+    chainId: base.id,
     value: 0,
     overrides: {
       gasLimit: 150000n,
@@ -94,32 +70,61 @@ const MintNFT = (): JSX.Element => {
   }
 
   const { isLoading, isSuccess } = useWaitForTransactionReceipt({
-    chainId: baseSepolia.id,
+    chainId: base.id,
     hash: hash as any,
     pollingInterval: 1_000,
   })
 
-  const updateArticlesCollectors = async () => {
-    // const NFTCollectors = await getArticlesCollectors(lesson.mirrorNFTAddress)
-    // NFTCollectors.reduce((p, c) => p + c?.tokenBalances?.length, 0)
-    // setNumberMinted(
-    //   NFTCollectors.reduce(
-    //     (p, c) => p + c?.tokenBalances?.length,
-    //     0
-    //   )?.toString()
-    // )
-
-    // TEMP
-    setNumberMinted('6')
+  const updateNFTCollectors = async () => {
+    const NFTCollectors = await getNFTsCollectors(NFTAddress)
+    NFTCollectors.reduce((p, c) => p + c?.tokenBalances?.length, 0)
+    setNumberMinted(
+      NFTCollectors.reduce(
+        (p, c) => p + c?.tokenBalances?.length,
+        0
+      )?.toString()
+    )
   }
+
+  const celebrateMint = () => {
+    toast.closeAll()
+    setShowConfetti(true)
+    // HACK: guess tokenId
+    const txLink = `https://opensea.io/assets/base/${NFTAddress}/${
+      1 + parseInt(numberMinted)
+    }`
+    toast({
+      description: (
+        <>
+          <Box>
+            <Box display="flex">
+              <Box mr="4">
+                <SealCheck width="40px" height="auto" />
+              </Box>
+              <Box flexDirection="column">
+                <Box>NFT minted:</Box>
+                <ExternalLink underline="true" href={txLink} alt="OpenSea Link">
+                  {isSmallScreen ? `${txLink.substring(0, 50)}...` : txLink}
+                </ExternalLink>
+              </Box>
+            </Box>
+          </Box>
+        </>
+      ),
+      status: 'success',
+      duration: 10000,
+      isClosable: true,
+    })
+  }
+
   useEffect(() => {
-    if (address) updateArticlesCollectors().catch(console.error)
-  }, [address])
+    updateNFTCollectors().catch(console.error)
+  }, [])
 
   useEffect(() => {
     if (isLoading) {
       toast.closeAll()
-      const txLink = `https://sepolia.basescan.org/tx/${hash}`
+      const txLink = `https://basescan.org/tx/${hash}`
       toast({
         description: (
           <>
@@ -151,49 +156,33 @@ const MintNFT = (): JSX.Element => {
 
   useEffect(() => {
     if (isSuccess) {
-      toast.closeAll()
-      setShowConfetti(true)
-      // HACK: guess tokenId
-      const txLink = `https://testnets.opensea.io/assets/base-sepolia/${NFTAddress}/${
-        1 + parseInt(numberMinted)
-      }`
-      toast({
-        description: (
-          <>
-            <Box>
-              <Box display="flex">
-                <Box mr="4">
-                  <SealCheck width="40px" height="auto" />
-                </Box>
-                <Box flexDirection="column">
-                  <Box>NFT minted:</Box>
-                  <ExternalLink
-                    underline="true"
-                    href={txLink}
-                    alt="OpenSea Link"
-                  >
-                    {isSmallScreen ? `${txLink.substring(0, 50)}...` : txLink}
-                  </ExternalLink>
-                </Box>
-              </Box>
-            </Box>
-          </>
-        ),
-        status: 'success',
-        duration: 10000,
-        isClosable: true,
-      })
+      celebrateMint()
     }
   }, [isSuccess])
 
-  const openInNewTab = (url) => {
-    window.open(url, '_blank', 'noopener,noreferrer')
-  }
+  const { data: availableCapabilities } = useCapabilities({
+    account: account.address,
+  })
+  const capabilities = useMemo(() => {
+    if (!availableCapabilities || !account.chainId) return {}
+    const capabilitiesForChain = availableCapabilities[account.chainId]
+    if (
+      capabilitiesForChain['paymasterService'] &&
+      capabilitiesForChain['paymasterService'].supported
+    ) {
+      return {
+        paymasterService: {
+          url: `${document.location.origin}/api/paymaster`,
+        },
+      }
+    }
+    return {}
+  }, [availableCapabilities, account.chainId])
 
   return (
     <Box maxW="500px" margin="auto" my="4">
       <Image
-        src="https://raw.seadn.io/files/973e1681b73e67c3b59c32401c9f1e71.gif"
+        src="https://i.seadn.io/s/raw/files/4102b70e716709c2189a036016ebe28c.gif"
         width="100%"
         height="100%"
         mb="4"
@@ -218,59 +207,117 @@ const MintNFT = (): JSX.Element => {
               if (!address) {
                 await open({ view: 'Connect' })
               } else if (numberMinted !== '-') {
-                if (parseInt(numberMinted) >= 100) {
-                  openInNewTab(
-                    `https://testnets.opensea.io/collection/smart-wallet-oe`
-                  )
-                } else {
-                  if (address && chain?.id !== baseSepolia.id) {
-                    try {
-                      await switchChain(wagmiConfig, {
-                        chainId: baseSepolia.id,
-                      })
-                    } catch (error) {
-                      console.error(error)
-                      toast({
-                        title: 'Switch your network to Base.',
-                        description: (
-                          <>Click Mint again after switching network.</>
-                        ),
-                        status: 'error',
-                        duration: 10000,
-                        isClosable: true,
-                      })
-                    }
-                    setIsMinting(false)
+                if (address && chain?.id !== base.id) {
+                  try {
+                    await switchChain(wagmiConfig, {
+                      chainId: base.id,
+                    })
+                  } catch (error) {
+                    console.error(error)
                     toast({
-                      title: 'The network has been switched to Base.',
-                      description: <>Click Mint again.</>,
-                      status: 'warning',
+                      title: 'Switch your network to Base.',
+                      description: (
+                        <>Click Mint again after switching network.</>
+                      ),
+                      status: 'error',
                       duration: 10000,
                       isClosable: true,
                     })
-                  } else if (!isMinting) {
-                    setIsMinting(true)
-                    setTimeout(() => {
-                      setIsMinting(false)
-                    }, 3000)
-                    if (mintingError !== '') {
-                      toast({
-                        title: '⚠️ Problem while minting...',
-                        description: (
-                          <>
-                            <Box>
-                              {mintingError?.includes('exceeds the balance')
-                                ? 'The total cost including gas fee exceeds your balance of ETH on Optimism.'
-                                : mintingError}
-                            </Box>
-                            <Box>Refresh the page before trying again.</Box>
-                          </>
-                        ),
-                        status: 'error',
-                        duration: null,
-                        isClosable: true,
+                  }
+                  setIsMinting(false)
+                  toast({
+                    title: 'The network has been switched to Base.',
+                    description: <>Click Mint again.</>,
+                    status: 'warning',
+                    duration: 10000,
+                    isClosable: true,
+                  })
+                } else if (!isMinting) {
+                  setIsMinting(true)
+                  setTimeout(() => {
+                    setIsMinting(false)
+                  }, 3000)
+                  if (mintingError !== '') {
+                    toast({
+                      title: '⚠️ Problem while minting...',
+                      description: (
+                        <>
+                          <Box>
+                            {mintingError?.includes('exceeds the balance')
+                              ? 'The total cost including gas fee exceeds your balance of ETH on Optimism.'
+                              : mintingError}
+                          </Box>
+                          <Box>Refresh the page before trying again.</Box>
+                        </>
+                      ),
+                      status: 'error',
+                      duration: null,
+                      isClosable: true,
+                    })
+                  } else {
+                    const code = await client.getBytecode({ address })
+                    console.log('code', code)
+                    if (
+                      account.connector.type === 'coinbaseWallet' &&
+                      code?.startsWith('0x')
+                    ) {
+                      // Coinbase Smart Account
+                      const paymasterResults = await api('/api/paymaster', {
+                        method: 'pm_getPaymasterStubData',
+                        params: [
+                          {
+                            sender: address,
+                            nonce: '0x2',
+                            initCode: '0x',
+                            callData: `0x34fcd5be00000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000002000000000000000000000000077b82880ab87bdd8910f1f6324b34752eeab96ff0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000006000000000000000000000000000000000000000000000000000000000000001a484bb1e42000000000000000000000000${address.substring(
+                              2
+                            )}0000000000000000000000000000000000000000000000000000000000000001000000000000000000000000eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000c00000000000000000000000000000000000000000000000000000000000000180000000000000000000000000000000000000000000000000000000000000008000000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000000000000000000000000000000eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee00000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`,
+                            callGasLimit: '0x19d8c',
+                            verificationGasLimit: '0x97df4',
+                            preVerificationGas: '0x58df9',
+                            maxFeePerGas: '0x13e2110',
+                            maxPriorityFeePerGas: '0xf4240',
+                          },
+                          NFTAddress,
+                          '0x2105',
+                          null,
+                        ],
                       })
+                      const errorDetails = paymasterResults?.data?.error
+                      if (errorDetails) {
+                        if (
+                          errorDetails?.includes(
+                            'address has maximum number of transactions defined in policy'
+                          )
+                        ) {
+                          alert(
+                            'You have already claimed your free sponsored mint.'
+                          )
+                        } else if (
+                          errorDetails?.includes(
+                            'address has maximum sponsorship cost defined in policy'
+                          )
+                        ) {
+                          alert(
+                            'Gas price is currently high, try again later to claim your gas free transaction.'
+                          )
+                        } else alert(errorDetails)
+                      } else {
+                        writeContracts({
+                          account: address,
+                          contracts: [
+                            {
+                              address: NFTAddress,
+                              abi: nftABI,
+                              functionName: 'claim',
+                              args: contractArgs,
+                            },
+                          ],
+                          capabilities,
+                        } as any)
+                      }
                     } else {
+                      // EOA
                       const { request } = await simulateContract(
                         wagmiConfig,
                         mintArg
@@ -306,7 +353,7 @@ const MintNFT = (): JSX.Element => {
           }}
         >
           <Box fontWeight="bold">Mint Free Smart Wallet NFT</Box>
-          <Box ml="2">{`(${numberCollected}/∞ minted)`}</Box>
+          <Box ml="2">{`(${numberMinted}/∞ minted)`}</Box>
         </Button>
       )}
       <Confetti
