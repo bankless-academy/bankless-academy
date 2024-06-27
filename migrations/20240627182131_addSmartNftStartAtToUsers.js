724182d5446dbb8bb5d20cac2b7cef8f48cbb30f
Didier Krux <didier@krux.co>
2024-06-27 21:06:35 +0100

    fix table

--- migrations/20240627182131_addSmartNftStartAtToUsers.js
+++ migrations/20240627182131_addSmartNftStartAtToUsers.js
@@ -1,13 +1,13 @@
 const { TABLES } = require('../db')
 
 exports.up = async function (knex) {
-  await knex.schema.table(TABLES.completions, (table) => {
+  await knex.schema.table(TABLES.users, (table) => {
     table.timestamp('smart_nft_start_at').nullable().defaultTo(null)
   })
 }
 
 exports.down = async function (knex) {
-  await knex.schema.table(TABLES.completions, (table) => {
+  await knex.schema.table(TABLES.users, (table) => {
     table.dropColumn('smart_nft_start_at')
   })
 }
