163f871cbb9a4ecb3e550d7d4b0e0bb2cd2d5247
Didier Krux <didier@krux.co>
2024-10-14 18:54:02 +0100

    improve image rewrite

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,36 @@
 /* eslint-disable no-console */
 import * as Sentry from '@sentry/nextjs';
-
-// middleware.ts
-// TODO: migrate to https://github.com/neet/next-composition
 import { NextResponse } from 'next/server'
 import { userAgent } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-export function middleware(request: NextRequest): NextResponse {
+export async function middleware(request: NextRequest): Promise<NextResponse> {
   const ipAddress = request.ip || 'local'
-  // Detect and redirect bots
   const ua = userAgent(request)
-  console.log('url', request.url)
-  console.log('userAgent', ua)
-  if (ua.ua.includes('python') || ua.ua.includes('curl')
-    //  || (
-    //   ua.ua.includes('Mozilla') && ua.ua.includes('Chrome')
-    // )
-  ) {
+  const { pathname } = request.nextUrl;
+
+  // Image handling middleware
+  if (pathname.match(/^\/images\/.*\.(png|jpg|jpeg|gif|webp)$/)) {
+    try {
+      const response = await fetch(request.url, { method: 'HEAD' });
+      if (response.status === 404) {
+        const slug = pathname.split('/')[2];
+        const type = pathname?.split('/')[3]?.split('-')[0];
+        const rewriteUrl = new URL(`/api/lesson-image?slug=${slug}&type=${type}`, request.url);
+        return NextResponse.rewrite(rewriteUrl);
+      }
+    } catch (error) {
+      console.error('Error checking image:', error);
+      // In case of an error, continue with the original request
+    }
+  }
+
+  // Detect and redirect bots
+  if (ua.ua.includes('python') || ua.ua.includes('curl')) {
     if (request.url.includes('/api/passport')) return NextResponse.next()
     else return NextResponse.redirect(new URL('/maintenance', request.url))
   }
+
   // disable API calls if maintenance in progress
   if (
     process.env.NEXT_PUBLIC_MAINTENANCE &&
@@ -28,7 +38,9 @@ export function middleware(request: NextRequest): NextResponse {
     ipAddress !== 'local'
   ) {
     return NextResponse.redirect(new URL('/maintenance', request.url))
-  } else return NextResponse.next()
+  }
+
+  return NextResponse.next()
 }
 
 // Wrap the middleware function with Sentry error tracking
@@ -41,5 +53,5 @@ Sentry.init({
 
 // Apply middleware only to specific routes
 export const config = {
-  matcher: ['/api/passport', '/api/mint-badge', '/api/validate-quest'],
+  matcher: ['/api/passport', '/api/mint-badge', '/api/validate-quest', '/images/:path*'],
 }
--- src/pages/api/lesson-image.ts
+++ src/pages/api/lesson-image.ts
@@ -1,18 +1,19 @@
-/* eslint-disable no-console */
 import type { NextApiRequest, NextApiResponse } from 'next'
 import LESSONS from 'constants/lessons'
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const { slug, type } = req.query
-  console.log('req.query', req.query)
 
-  if (type === 'social') {
-    // https://ba.krux.dev/images/gitcoin-2.0-essentials/social-255bb483.png
-    // https://app.banklessacademy.com/images/gitcoin-2.0-essentials/social-255bb483.png
-    // /api/lesson-image?slug=gitcoin-2.0-essentials&type=social
-    console.log('slug', slug)
-    const imagePath = LESSONS.find((lesson) => lesson.slug === slug)?.socialImageLink
-    // redirect to imagePath
-    res.redirect(imagePath)
+  const imageType = type === 'social' ? 'socialImageLink' : type === 'lesson' ? 'lessonImageLink' : null
+
+  if (!imageType) {
+    res.status(404).send('Not found')
+  }
+  const imagePath = LESSONS.find((lesson) => lesson.slug === slug)?.[imageType]
+
+  if (imagePath) {
+    res.status(307).redirect(imagePath)
+  } else {
+    res.status(404).send('Not found')
   }
 }
