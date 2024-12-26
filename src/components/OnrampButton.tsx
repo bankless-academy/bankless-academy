fbfeb082ac30ae763cdb64e5b4f9742d6b393e25
Didier Krux <didier@krux.co>
2024-12-26 17:58:01 +0100

    update OnrampButton appId

--- src/components/OnrampButton.tsx
+++ src/components/OnrampButton.tsx
@@ -8,8 +8,9 @@ const OnrampButton = ({
   [key: string]: any
 }) => {
   const handleOnClick = () => {
+    // https://docs.cdp.coinbase.com/onramp/docs/api-onramp-initializing
     window.open(
-      `https://pay.coinbase.com/buy/select-asset?appId=bf18c88d-495a-463b-b249-0b9d3656cf5e&destinationWallets=%5B%7B%22address%22%3A%22${address}%22%2C%22blockchains%22%3A%5B%22optimism%22%2C%22base%22%5D%2C%22assets%22%3A%5B%5D%7D%5D&partnerUserId=0xe1887fF140BfA9D3b45D0B2077b7471124acD242&defaultNetwork=base&defaultExperience=send`,
+      `https://pay.coinbase.com/buy/select-asset?appId=f4210163-1e8b-4cc8-ad4a-45aab3d2a471&destinationWallets=%5B%7B%22address%22%3A%22${address}%22%2C%22blockchains%22%3A%5B%22optimism%22%2C%22base%22%5D%2C%22assets%22%3A%5B%22ETH%22%5D%7D%5D&defaultNetwork=base&defaultExperience=send`,
       '_blank',
       'width=470,height=750'
     )
