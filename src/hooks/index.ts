a7d2598f32d0de98bf4e2afe463c1678dc6edd21
Didier Krux <didier@krux.co>
2025-01-09 15:32:07 +0000

    fix useMediaQuery w/ ssr

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,8 +1,10 @@
 import { useEffect, useState } from 'react'
 import { useMediaQuery } from '@chakra-ui/react'
 
-export function useSmallScreen(): boolean[] {
-  return useMediaQuery(['(max-width: 800px)', '(max-width: 1260px)', '(max-width: 500px)'])
+export function useSmallScreen(): boolean[] | null {
+  return useMediaQuery(['(max-width: 800px)', '(max-width: 1260px)', '(max-width: 500px)'], {
+    ssr: false,
+  })
 }
 
 export const useWindowScrollPositions = () => {
