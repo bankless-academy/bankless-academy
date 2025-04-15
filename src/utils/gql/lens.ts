930a42a7b699a030e17ef133f4a0955b29301fba
Didier Krux <didier@krux.co>
2025-04-15 19:39:55 +0100

    fix LENS_API_URL naming

--- src/utils/gql/lens.ts
+++ src/utils/gql/lens.ts
@@ -1,6 +1,6 @@
 import { GraphQLClient } from "graphql-request";
 
-const AIRSTACK_API_URL = "https://api-v2.lens.dev/";
+const LENS_API_URL = "https://api-v2.lens.dev/";
 
-export const lensGraphQLClient = new GraphQLClient(AIRSTACK_API_URL);
+export const lensGraphQLClient = new GraphQLClient(LENS_API_URL);
 
